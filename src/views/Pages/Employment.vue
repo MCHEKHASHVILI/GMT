ac270afdc24b88105fd84e3e29905e07553fd45b
Rikudo10 <giorgisamxaradze16@gmail.com>
2023-04-13 01:40:02 +0400

    Finished Markup

--- src/views/Pages/Employment.vue
+++ src/views/Pages/Employment.vue
@@ -6,59 +6,64 @@ import mainLayout from '../../layouts/mainLayout.vue';
     <h1 class="pl-[150px] text-center font-bold text-3xl pb-12">EMPLOYMENT</h1>
     <div class="container ">
       <div class="justify-center flex gap-[15px] pb-1.5 ml-[150px]">
-    <div class="w-[550px] p-6 bg-white border border-gray-300  ">
-      <div class="flex">
-        <img class="h-12" src="https://www.gmt.ge/uploads_script/news/596e27ljb1c75l4.jpg">
-        <div>
-          <h2 class="font-medium md:font-bold">FINICULAR</h2>
-          <h2 class="font-medium md:font-bold">Chef</h2>
-        </div>
-        <div class="py-6 ml-[190px]">
-          <h2 class="font-medium md:font-bold">4000 ₾ per month</h2>
-        </div>
-      </div>
-      <p class="mb-3 font-normal text-gray-700 dark:text-black">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae amet veniam, est nisi alias nobis eligendi quo odio asperiores corrupti quibusdam beatae dolorem dolore. Unde fugit consectetur placeat ullam eaque?</p>
-      <div class="flex">
-        <div>
-          <p class="pb-1.5"><span class="font-medium md:font-bold">Full time</span> 00:09-00:17:00</p>
-          <p>march 25, 2023 - 10:25</p>
-        </div>
-        <div class="ml-[180px]">
-          <a href="#" class="inline-flex items-center px-[25px] py-3 text-sm  text-center text-black bg-zinc-200">
-            VIEW DETAILS
-          </a>
-        </div>
-      </div>
-    </div>
-      <div class="w-[550px] pb-1.5	 p-6 bg-white border border-gray-300  ">
-        <div class="flex">
-          <img class="h-12" src="https://www.gmt.ge/uploads_script/news/596e27ljb1c75l4.jpg">
-          <div>
-            <h2 class="font-medium md:font-bold">FINICULAR</h2>
-            <h2 class="font-medium md:font-bold">Chef</h2>
+        <div class="w-[550px] p-6 bg-white border border-gray-300  ">
+          <div class="flex">
+            <img class="h-12" src="https://www.gmt.ge/uploads_script/news/596e27ljb1c75l4.jpg">
+            <div>
+              <h2 class="font-medium md:font-bold">FINICULAR</h2>
+              <h2 class="font-medium md:font-bold">Chef</h2>
+            </div>
+            <div class="py-6 ml-[190px]">
+              <h2 class="font-medium md:font-bold">4000 ₾ per month</h2>
+            </div>
           </div>
-          <div class="py-6 ml-[190px]">
-            <h2 class="font-medium md:font-bold">4000 ₾ per month</h2>
+          <p class="mb-3 font-normal text-gray-700 dark:text-black">Lorem ipsum, dolor sit amet consectetur adipisicing
+            elit. Molestiae amet veniam, est nisi alias nobis eligendi quo odio asperiores corrupti quibusdam beatae
+            dolorem dolore. Unde fugit consectetur placeat ullam eaque?</p>
+          <div class="flex">
+            <div>
+              <p class="pb-1.5"><span class="font-medium md:font-bold">Full time</span> 00:09-00:17:00</p>
+              <p>march 25, 2023 - 10:25</p>
+            </div>
+            <div class="ml-[180px]">
+              <a href="#" class="inline-flex items-center px-[25px] py-3 text-sm  text-center text-black bg-zinc-200">
+                VIEW DETAILS
+              </a>
+            </div>
           </div>
         </div>
-        <p class="mb-3 font-normal text-gray-700 dark:text-black">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae amet veniam, est nisi alias nobis eligendi quo odio asperiores corrupti quibusdam beatae dolorem dolore. Unde fugit consectetur placeat ullam eaque?</p>
-        <div class="flex">
-          <div>
-            <p class="pb-1.5"><span class="font-medium md:font-bold">Full time</span> 00:09-00:17:00</p>
-            <p>march 25, 2023 - 10:25</p>
+        <div class="w-[550px] pb-1.5	 p-6 bg-white border border-gray-300  ">
+          <div class="flex">
+            <img class="h-12" src="https://www.gmt.ge/uploads_script/news/596e27ljb1c75l4.jpg">
+            <div>
+              <h2 class="font-medium md:font-bold">FINICULAR</h2>
+              <h2 class="font-medium md:font-bold">Chef</h2>
+            </div>
+            <div class="py-6 ml-[190px]">
+              <h2 class="font-medium md:font-bold">4000 ₾ per month</h2>
+            </div>
           </div>
-          <div class="ml-[180px]">
-            <a href="#" class="inline-flex items-center px-[25px] py-3 text-sm  text-center text-black bg-zinc-200">
-              VIEW DETAILS
-            </a>
+          <p class="mb-3 font-normal text-gray-700 dark:text-black">Lorem ipsum, dolor sit amet consectetur adipisicing
+            elit. Molestiae amet veniam, est nisi alias nobis eligendi quo odio asperiores corrupti quibusdam beatae
+            dolorem dolore. Unde fugit consectetur placeat ullam eaque?</p>
+          <div class="flex">
+            <div>
+              <p class="pb-1.5"><span class="font-medium md:font-bold">Full time</span> 00:09-00:17:00</p>
+              <p>march 25, 2023 - 10:25</p>
+            </div>
+            <div class="ml-[180px]">
+              <a href="#" class="inline-flex items-center px-[25px] py-3 text-sm  text-center text-black bg-zinc-200">
+                VIEW DETAILS
+              </a>
+            </div>
           </div>
         </div>
-      </div>
       </div>
       <div class="flex justify-center	 gap-[15px] pb-1.5 ml-[150px]">
         <div class="w-[550px] p-6 bg-white border border-gray-300  ">
           <div class="flex">
-            <img class="h-12" src="https://scontent.ftbs5-2.fna.fbcdn.net/v/t1.6435-9/183461071_101827522088439_4050325639092768269_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGTGt73bVV_l8Bs9YITRHfYWR48Tc03CnxZHjxNzTcKfChcP4FfLTCMVrH_1JNdVuYkRe5QIU9W9xCFmkZfU2Z3&_nc_ohc=L5UBt4mwf9EAX_EDPH0&_nc_ht=scontent.ftbs5-2.fna&oh=00_AfC8c9Qx-Qm6-ErbAxLW3gokBrv10RB-TjzfyH85r5DIKg&oe=645E91D1">
+            <img class="h-12"
+              src="https://scontent.ftbs5-2.fna.fbcdn.net/v/t1.6435-9/183461071_101827522088439_4050325639092768269_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGTGt73bVV_l8Bs9YITRHfYWR48Tc03CnxZHjxNzTcKfChcP4FfLTCMVrH_1JNdVuYkRe5QIU9W9xCFmkZfU2Z3&_nc_ohc=L5UBt4mwf9EAX_EDPH0&_nc_ht=scontent.ftbs5-2.fna&oh=00_AfC8c9Qx-Qm6-ErbAxLW3gokBrv10RB-TjzfyH85r5DIKg&oe=645E91D1">
             <div>
               <h2 class="font-medium md:font-bold">PURI GULIANI</h2>
               <h2 class="font-medium md:font-bold">Chef</h2>
@@ -67,7 +72,9 @@ import mainLayout from '../../layouts/mainLayout.vue';
               <h2 class="font-medium md:font-bold">4000 ₾ per month</h2>
             </div>
           </div>
-          <p class="mb-3 font-normal text-gray-700 dark:text-black">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae amet veniam, est nisi alias nobis eligendi quo odio asperiores corrupti quibusdam beatae dolorem dolore. Unde fugit consectetur placeat ullam eaque?</p>
+          <p class="mb-3 font-normal text-gray-700 dark:text-black">Lorem ipsum, dolor sit amet consectetur adipisicing
+            elit. Molestiae amet veniam, est nisi alias nobis eligendi quo odio asperiores corrupti quibusdam beatae
+            dolorem dolore. Unde fugit consectetur placeat ullam eaque?</p>
           <div class="flex">
             <div>
               <p class="pb-1.5"><span class="font-medium md:font-bold">Full time</span> 00:09-00:17:00</p>
@@ -82,7 +89,8 @@ import mainLayout from '../../layouts/mainLayout.vue';
         </div>
         <div class="w-[550px] p-6 bg-white border border-gray-300 ">
           <div class="flex">
-            <img class="h-12" src="https://scontent.ftbs5-2.fna.fbcdn.net/v/t1.6435-9/183461071_101827522088439_4050325639092768269_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGTGt73bVV_l8Bs9YITRHfYWR48Tc03CnxZHjxNzTcKfChcP4FfLTCMVrH_1JNdVuYkRe5QIU9W9xCFmkZfU2Z3&_nc_ohc=L5UBt4mwf9EAX_EDPH0&_nc_ht=scontent.ftbs5-2.fna&oh=00_AfC8c9Qx-Qm6-ErbAxLW3gokBrv10RB-TjzfyH85r5DIKg&oe=645E91D1">
+            <img class="h-12"
+              src="https://scontent.ftbs5-2.fna.fbcdn.net/v/t1.6435-9/183461071_101827522088439_4050325639092768269_n.jpg?_nc_cat=110&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeGTGt73bVV_l8Bs9YITRHfYWR48Tc03CnxZHjxNzTcKfChcP4FfLTCMVrH_1JNdVuYkRe5QIU9W9xCFmkZfU2Z3&_nc_ohc=L5UBt4mwf9EAX_EDPH0&_nc_ht=scontent.ftbs5-2.fna&oh=00_AfC8c9Qx-Qm6-ErbAxLW3gokBrv10RB-TjzfyH85r5DIKg&oe=645E91D1">
             <div>
               <h2 class="font-medium md:font-bold">PURI GULIANI</h2>
               <h2 class="font-medium md:font-bold">Chef</h2>
@@ -91,7 +99,9 @@ import mainLayout from '../../layouts/mainLayout.vue';
               <h2 class="font-medium md:font-bold">4000 ₾ per month</h2>
             </div>
           </div>
-          <p class="mb-3 font-normal text-gray-700 dark:text-black">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae amet veniam, est nisi alias nobis eligendi quo odio asperiores corrupti quibusdam beatae dolorem dolore. Unde fugit consectetur placeat ullam eaque?</p>
+          <p class="mb-3 font-normal text-gray-700 dark:text-black">Lorem ipsum, dolor sit amet consectetur adipisicing
+            elit. Molestiae amet veniam, est nisi alias nobis eligendi quo odio asperiores corrupti quibusdam beatae
+            dolorem dolore. Unde fugit consectetur placeat ullam eaque?</p>
           <div class="flex">
             <div>
               <p class="pb-1.5"><span class="font-medium md:font-bold">Full time</span> 00:09-00:17:00</p>
@@ -108,7 +118,8 @@ import mainLayout from '../../layouts/mainLayout.vue';
       <div class="flex justify-center	 gap-[15px] ml-[150px]">
         <div class="w-[550px] p-6 bg-white border border-gray-300  ">
           <div class="flex">
-            <img class="h-12" src="https://scontent.ftbs5-3.fna.fbcdn.net/v/t1.6435-9/119485246_609465163063254_3941978344519784791_n.png?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeF7BLgwMIY7ScJ5lcI0NVZrfWO6Ghl5-p59Y7oaGXn6nuoHjoskUxA-ZTex2F9AZ9qmjNfia4sqGiNXUCuc8Gdh&_nc_ohc=YYSVYk6S3Y8AX-Wi4Z-&_nc_ht=scontent.ftbs5-3.fna&oh=00_AfDG8r9IZCfI7vVnmpgCogo-J2Xx-ShUnLjK_f5ButC9Bg&oe=645EA161">
+            <img class="h-12"
+              src="https://scontent.ftbs5-3.fna.fbcdn.net/v/t1.6435-9/119485246_609465163063254_3941978344519784791_n.png?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeF7BLgwMIY7ScJ5lcI0NVZrfWO6Ghl5-p59Y7oaGXn6nuoHjoskUxA-ZTex2F9AZ9qmjNfia4sqGiNXUCuc8Gdh&_nc_ohc=YYSVYk6S3Y8AX-Wi4Z-&_nc_ht=scontent.ftbs5-3.fna&oh=00_AfDG8r9IZCfI7vVnmpgCogo-J2Xx-ShUnLjK_f5ButC9Bg&oe=645EA161">
             <div>
               <h2 class="font-medium md:font-bold">RIGI GASTRODOUQAN</h2>
               <h2 class="font-medium md:font-bold">Chef</h2>
@@ -117,7 +128,9 @@ import mainLayout from '../../layouts/mainLayout.vue';
               <h2 class="font-medium md:font-bold">4000 ₾ per month</h2>
             </div>
           </div>
-          <p class="mb-3 font-normal text-gray-700 dark:text-black">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae amet veniam, est nisi alias nobis eligendi quo odio asperiores corrupti quibusdam beatae dolorem dolore. Unde fugit consectetur placeat ullam eaque?</p>
+          <p class="mb-3 font-normal text-gray-700 dark:text-black">Lorem ipsum, dolor sit amet consectetur adipisicing
+            elit. Molestiae amet veniam, est nisi alias nobis eligendi quo odio asperiores corrupti quibusdam beatae
+            dolorem dolore. Unde fugit consectetur placeat ullam eaque?</p>
           <div class="flex">
             <div>
               <p class="pb-1.5"><span class="font-medium md:font-bold">Full time</span> 00:09-00:17:00</p>
@@ -132,7 +145,8 @@ import mainLayout from '../../layouts/mainLayout.vue';
         </div>
         <div class="w-[550px] p-6 bg-white border border-gray-300  ">
           <div class="flex">
-            <img class="h-12" src="https://scontent.ftbs5-3.fna.fbcdn.net/v/t1.6435-9/119485246_609465163063254_3941978344519784791_n.png?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeF7BLgwMIY7ScJ5lcI0NVZrfWO6Ghl5-p59Y7oaGXn6nuoHjoskUxA-ZTex2F9AZ9qmjNfia4sqGiNXUCuc8Gdh&_nc_ohc=YYSVYk6S3Y8AX-Wi4Z-&_nc_ht=scontent.ftbs5-3.fna&oh=00_AfDG8r9IZCfI7vVnmpgCogo-J2Xx-ShUnLjK_f5ButC9Bg&oe=645EA161">
+            <img class="h-12"
+              src="https://scontent.ftbs5-3.fna.fbcdn.net/v/t1.6435-9/119485246_609465163063254_3941978344519784791_n.png?_nc_cat=106&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeF7BLgwMIY7ScJ5lcI0NVZrfWO6Ghl5-p59Y7oaGXn6nuoHjoskUxA-ZTex2F9AZ9qmjNfia4sqGiNXUCuc8Gdh&_nc_ohc=YYSVYk6S3Y8AX-Wi4Z-&_nc_ht=scontent.ftbs5-3.fna&oh=00_AfDG8r9IZCfI7vVnmpgCogo-J2Xx-ShUnLjK_f5ButC9Bg&oe=645EA161">
             <div>
               <h2 class="font-medium md:font-bold">RIGI GASTRODOUQAN</h2>
               <h2 class="font-medium md:font-bold">Chef</h2>
@@ -141,20 +155,21 @@ import mainLayout from '../../layouts/mainLayout.vue';
               <h2 class="font-medium md:font-bold">4000 ₾ per month</h2>
             </div>
           </div>
-          <p class="mb-3 font-normal text-gray-700 dark:text-black">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Molestiae amet veniam, est nisi alias nobis eligendi quo odio asperiores corrupti quibusdam beatae dolorem dolore. Unde fugit consectetur placeat ullam eaque?</p>
+          <p class="mb-3 font-normal text-gray-700 dark:text-black">Lorem ipsum, dolor sit amet consectetur adipisicing
+            elit. Molestiae amet veniam, est nisi alias nobis eligendi quo odio asperiores corrupti quibusdam beatae
+            dolorem dolore. Unde fugit consectetur placeat ullam eaque?</p>
           <div class="flex">
-            <div>
-              <p class="pb-1.5"><span class="font-medium md:font-bold">Full time</span> 00:09-00:17:00</p>
-              <p>march 25, 2023 - 10:25</p>
-            </div>
-            <div class="ml-[180px]">
-              <a href="#" class="inline-flex items-center px-[25px] py-3 text-sm text-center text-black bg-zinc-200">
-                VIEW DETAILS
-              </a>
-            </div>
+          <div>
+            <p class="pb-1.5"><span class="font-medium md:font-bold">Full time</span> 00:09-00:17:00</p>
+            <p>march 25, 2023 - 10:25</p>
+          </div>
+          <div class="ml-[180px]">
+            <a href="#" class="inline-flex items-center px-[25px] py-3 text-sm text-center text-black bg-zinc-200">
+              VIEW DETAILS
+            </a>
           </div>
         </div>
       </div>
+    </div>
   </div>
-  </main-layout>
-</template>
+</main-layout></template>
