1adc0291f5f13076953c287a6a1c405d33f5158c
Gandobilis <lashadeveloper@gmail.com>
2023-04-13 14:20:24 +0400

    HTML structure constructed

---
+++ src/views/Pages/EmploymentContent.vue
@@ -0,0 +1,83 @@
+<script setup></script>
+
+<template>
+  <div>
+    <div>
+      <div>
+        <img
+          src="https://scontent.ftbs5-3.fna.fbcdn.net/v/t1.6435-9/36342042_994746297362019_5087005955971874816_n.png?_nc_cat=102&ccb=1-7&_nc_sid=09cbfe&_nc_ohc=K9cjnCANAxgAX8KUgeS&_nc_ht=scontent.ftbs5-3.fna&oh=00_AfB-jhcm2EwT3MLGYNy4C5GBrpG_iSw5AjR5zmHHdhgOrA&oe=645F3224"
+          alt="Employer logo"
+        />
+      </div>
+      <div>
+        <h1>FUNICULAR</h1>
+        <h3>Chef</h3>
+      </div>
+    </div>
+    <div>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatum
+        veritatis architecto optio possimus id praesentium unde quasi
+        consequatur obcaecati sed eligendi vitae perspiciatis corporis amet
+        officiis culpa explicabo, sit ratione dolore odio doloribus! Quisquam
+        autem qui ab voluptate est dolorum explicabo blanditiis eveniet commodi,
+        iste eligendi cum distinctio. Nam, corrupti.
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus
+        minima rerum odit optio error mollitia? Recusandae numquam consequuntur
+        at maiores optio dolor voluptatem harum aut inventore rem eaque fugit
+        fugiat tempore repellat sint, molestias sapiente ut nihil delectus!
+        Cumque sint nemo debitis. Dicta vero dolor commodi omnis aut deleniti
+        cumque ullam perspiciatis. Ipsum, qui atque!
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Exercitationem
+        laborum, suscipit nihil voluptatibus veritatis corrupti autem sapiente.
+        Voluptates, cumque nostrum delectus quos earum a suscipit distinctio?
+        Laboriosam praesentium fugit enim earum suscipit quos dolore cum dolorem
+        optio tempora temporibus neque porro possimus doloribus animi beatae
+        nemo sit ipsa eligendi quas ratione, rerum fugiat odio? Quod possimus
+        maiores consectetur iste amet voluptate nostrum iure aliquid inventore
+        fuga quisquam dolor ipsa velit totam blanditiis sed eaque, fugiat odit
+        dignissimos ratione recusandae veniam dolorum doloribus? Magnam labore
+        nobis sint nam illo minima iste qui repudiandae numquam illum, aut vel
+        quidem modi saepe voluptatem reiciendis voluptatum ut. Officiis totam
+        laboriosam repellendus perferendis quasi ipsa suscipit, dolore velit
+        assumenda minima nisi, reprehenderit eius cumque ipsam magnam in nam
+        vitae debitis molestias ratione atque! Doloremque, perferendis
+        reiciendis repellat necessitatibus sequi numquam ad corrupti fugit
+        asperiores dolorem, nisi velit odio autem facere. Voluptatibus quibusdam
+        reprehenderit delectus quasi quas id magni sunt odio dolor, nemo qui
+        impedit labore. Ab officia facere, in id deleniti aspernatur dolore
+        laborum, atque enim tempora eius? Magnam eius reprehenderit, assumenda
+        error repellat ea?
+      </p>
+      <br />
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Unde, vel
+        blanditiis alias itaque asperiores similique perspiciatis nulla sunt
+        laborum optio porro cum consectetur temporibus ipsam!
+      </p>
+    </div>
+    <div>
+      <div>
+        <p>Salary</p>
+        <p>4000 ₾ per month</p>
+      </div>
+      <div>
+        <p>Full time</p>
+        <p>00:09-00:17:00</p>
+      </div>
+      <div>
+        <p>Contact</p>
+        <p>GMTHospitality@GMT.GE</p>
+      </div>
+      <div>
+        <p>match 25, 2023 &#183; 10:25</p>
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/Pages/Home.vue
+++ src/views/Pages/Home.vue
@@ -1,9 +1,10 @@
 <script setup>
 import mainLayout from '../../layouts/mainLayout.vue';
+import EmploymentContent from './EmploymentContent.vue';
 </script>
 <template>
   <main-layout>
-    <p>
+    <!--<p>
     Home Page Content
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad maiores iure provident delectus praesentium labore nihil in veniam a totam quasi sunt aspernatur iusto architecto, vero perferendis modi suscipit vitae.
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad maiores iure provident delectus praesentium labore nihil in veniam a totam quasi sunt aspernatur iusto architecto, vero perferendis modi suscipit vitae.
@@ -12,5 +13,7 @@ import mainLayout from '../../layouts/mainLayout.vue';
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad maiores iure provident delectus praesentium labore nihil in veniam a totam quasi sunt aspernatur iusto architecto, vero perferendis modi suscipit vitae.
     Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad maiores iure provident delectus praesentium labore nihil in veniam a totam quasi sunt aspernatur iusto architecto, vero perferendis modi suscipit vitae.
     </p>
+    -->
+    <employment-content></employment-content>
   </main-layout>
 </template>
