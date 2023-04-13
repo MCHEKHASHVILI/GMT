45a2a45adc6a076e391af7543152890bf4f9f8c3
zurikelass <zuragogberashvili93@gmail.com>
2023-04-13 14:58:06 +0400

    Make cards without tags

--- src/views/Pages/Events.vue
+++ src/views/Pages/Events.vue
@@ -13,8 +13,11 @@ import mainLayout from '../../layouts/mainLayout.vue';
          <div class="rounded-xl overflow-hidden"> 
           <img src="./images/jazz.jpg" alt="">
          </div>
-          <h5 class="text-2xl md:text-3xl font-medium mt-3">Jazz up your night:
+          <h5 class="text-2xl md:text-xl font-medium mt-3">Jazz up your night:
            Join us for live music and delicious dinning at our restaurant!</h5>
+           <h6></h6>
+           <div href="#" class="text-center bg-blue-100 text-black-700 py-2 rounded-lg
+           font-semibold mt-4 hover:bg-grey-300 focus:scale-95 transition-all duration-200 ease-out">VIEW DETAILS</div>
          </div>
         </div>
       </div>
