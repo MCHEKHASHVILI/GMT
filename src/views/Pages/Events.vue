9c5308149cb7d1f3aa5a104592d34ec0f740a361
zurikelass <zuragogberashvili93@gmail.com>
2023-04-13 15:07:33 +0400

    Card is Done

--- src/views/Pages/Events.vue
+++ src/views/Pages/Events.vue
@@ -15,12 +15,98 @@ import mainLayout from '../../layouts/mainLayout.vue';
          </div>
           <h5 class="text-2xl md:text-xl font-medium mt-3">Jazz up your night:
            Join us for live music and delicious dinning at our restaurant!</h5>
-           <h6></h6>
+           <div class="text-center px-6 pt-4 pb-2">
+    <span class="inline-block bg-gray-200 rounded px-8 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Jazz</span>
+    <span class="inline-block bg-gray-50 rounded px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Price: 150 Gel</span>
+    <span class="inline-block bg-gray-50 rounded px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">March 25,2023 | 10:25</span>
+  </div>
+           <div href="#" class="text-center bg-blue-100 text-black-700 py-2 rounded-lg
+           font-semibold mt-4 hover:bg-grey-300 focus:scale-95 transition-all duration-200 ease-out">VIEW DETAILS</div>
+         </div>
+        </div>
+        <div class="rounded-xl shadow-lg">
+         <div class="p-5 flex flex-col">
+         <div class="rounded-xl overflow-hidden"> 
+          <img src="./images/jazz.jpg" alt="">
+         </div>
+          <h5 class="text-2xl md:text-xl font-medium mt-3">Jazz up your night:
+           Join us for live music and delicious dinning at our restaurant!</h5>
+           <div class="text-center px-6 pt-4 pb-2">
+    <span class="inline-block bg-gray-200 rounded px-8 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Jazz</span>
+    <span class="inline-block bg-gray-50 rounded px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Price: 150 Gel</span>
+    <span class="inline-block bg-gray-50 rounded px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">March 25,2023 | 10:25</span>
+  </div>
+           <div href="#" class="text-center bg-blue-100 text-black-700 py-2 rounded-lg
+           font-semibold mt-4 hover:bg-grey-300 focus:scale-95 transition-all duration-200 ease-out">VIEW DETAILS</div>
+         </div>
+        </div>
+        <div class="rounded-xl shadow-lg">
+         <div class="p-5 flex flex-col">
+         <div class="rounded-xl overflow-hidden"> 
+          <img src="./images/jazz.jpg" alt="">
+         </div>
+          <h5 class="text-2xl md:text-xl font-medium mt-3">Jazz up your night:
+           Join us for live music and delicious dinning at our restaurant!</h5>
+           <div class="text-center px-6 pt-4 pb-2">
+    <span class="inline-block bg-gray-200 rounded px-8 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Jazz</span>
+    <span class="inline-block bg-gray-50 rounded px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Price: 150 Gel</span>
+    <span class="inline-block bg-gray-50 rounded px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">March 25,2023 | 10:25</span>
+  </div>
+           <div href="#" class="text-center bg-blue-100 text-black-700 py-2 rounded-lg
+           font-semibold mt-4 hover:bg-grey-300 focus:scale-95 transition-all duration-200 ease-out">VIEW DETAILS</div>
+         </div>
+        </div>
+        <div class="rounded-xl shadow-lg">
+         <div class="p-5 flex flex-col">
+         <div class="rounded-xl overflow-hidden"> 
+          <img src="./images/jazz.jpg" alt="">
+         </div>
+          <h5 class="text-2xl md:text-xl font-medium mt-3">Jazz up your night:
+           Join us for live music and delicious dinning at our restaurant!</h5>
+           <div class="text-center px-6 pt-4 pb-2">
+    <span class="inline-block bg-gray-200 rounded px-8 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Jazz</span>
+    <span class="inline-block bg-gray-50 rounded px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Price: 150 Gel</span>
+    <span class="inline-block bg-gray-50 rounded px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">March 25,2023 | 10:25</span>
+  </div>
+           <div href="#" class="text-center bg-blue-100 text-black-700 py-2 rounded-lg
+           font-semibold mt-4 hover:bg-grey-300 focus:scale-95 transition-all duration-200 ease-out">VIEW DETAILS</div>
+         </div>
+        </div>
+        <div class="rounded-xl shadow-lg">
+         <div class="p-5 flex flex-col">
+         <div class="rounded-xl overflow-hidden"> 
+          <img src="./images/jazz.jpg" alt="">
+         </div>
+          <h5 class="text-2xl md:text-xl font-medium mt-3">Jazz up your night:
+           Join us for live music and delicious dinning at our restaurant!</h5>
+           <div class="text-center px-6 pt-4 pb-2">
+    <span class="inline-block bg-gray-200 rounded px-8 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Jazz</span>
+    <span class="inline-block bg-gray-50 rounded px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Price: 150 Gel</span>
+    <span class="inline-block bg-gray-50 rounded px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">March 25,2023 | 10:25</span>
+  </div>
+           <div href="#" class="text-center bg-blue-100 text-black-700 py-2 rounded-lg
+           font-semibold mt-4 hover:bg-grey-300 focus:scale-95 transition-all duration-200 ease-out">VIEW DETAILS</div>
+         </div>
+        </div>
+        <div class="rounded-xl shadow-lg">
+         <div class="p-5 flex flex-col">
+         <div class="rounded-xl overflow-hidden"> 
+          <img src="./images/jazz.jpg" alt="">
+         </div>
+          <h5 class="text-2xl md:text-xl font-medium mt-3">Jazz up your night:
+           Join us for live music and delicious dinning at our restaurant!</h5>
+           <div class="text-center px-6 pt-4 pb-2">
+    <span class="inline-block bg-gray-200 rounded px-8 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Jazz</span>
+    <span class="inline-block bg-gray-50 rounded px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">Price: 150 Gel</span>
+    <span class="inline-block bg-gray-50 rounded px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">March 25,2023 | 10:25</span>
+  </div>
            <div href="#" class="text-center bg-blue-100 text-black-700 py-2 rounded-lg
            font-semibold mt-4 hover:bg-grey-300 focus:scale-95 transition-all duration-200 ease-out">VIEW DETAILS</div>
          </div>
         </div>
       </div>
    </div>
+   
+   
   </main-layout>
 </template>
