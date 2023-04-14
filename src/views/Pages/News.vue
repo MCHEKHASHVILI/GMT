7a42fee869ec79e0157ea706c1cbc10f984e9989
AstralBoy <baratashvili.beka@kiu.edu.ge>
2023-04-14 22:30:49 +0400

    Added date and view

--- src/views/Pages/News.vue
+++ src/views/Pages/News.vue
@@ -7,104 +7,177 @@
 
     <div class="grid grid-cols-3 gap-10">
         <div class="mx-auto max-w-md flex flex-col items-center">
-            <div class="flex flex-col items-center">
-                <img src="https://picsum.photos/400/300?random=" alt="Placeholder Image"
-                    class="w-full h-40 object-cover mb-4">
-                <p class="text-gray-600 text-sm leading-5">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque mi ac nisl lacinia, vitae
-                    ullamcorper eros faucibus. Duis ullamcorper hendrerit urna, sit amet semper felis dictum quis. Vivamus
-                    nec velit eu nibh vestibulum commodo ac felis blandit varius vitae sed sapien.
-                </p>
+            <div class="relative">
+                <img src="https://picsum.photos/213/160?random=" alt="Placeholder Image"
+                    class="w-full h-40 object-cover mb-4" style="width: 448px; height: 160px;">
+                <button
+                    class="absolute bottom-0 left-0 bg-transparent hover:bg-gray-500 hover:text-white font-bold py-2 px-4 rounded">
+                    01/01/2023
+                </button>
+                <button
+                    class="absolute bottom-0 right-0 bg-transparent hover:bg-gray-500 hover:text-white font-bold py-2 px-4 rounded">
+                    100k views
+                </button>
             </div>
+            <p class="text-gray-600 text-sm leading-5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque mi ac nisl lacinia, vitae
+                ullamcorper eros faucibus. Duis ullamcorper hendrerit urna, sit amet semper felis dictum quis. Vivamus nec
+                velit eu nibh vestibulum commodo ac felis blandit varius vitae sed sapien.
+            </p>
         </div>
         <div class="mx-auto max-w-md flex flex-col items-center">
-            <div class="flex flex-col items-center">
-                <img src="https://picsum.photos/400/300?random=" alt="Placeholder Image"
-                    class="w-full h-40 object-cover mb-4">
-                <p class="text-gray-600 text-sm leading-5">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque mi ac nisl lacinia, vitae
-                    ullamcorper eros faucibus. Duis ullamcorper hendrerit urna, sit amet semper felis dictum quis. Vivamus
-                    nec velit eu nibh vestibulum commodo ac felis blandit varius vitae sed sapien.
-                </p>
+            <div class="relative">
+                <img src="https://picsum.photos/213/160?random=" alt="Placeholder Image"
+                    class="w-full h-40 object-cover mb-4" style="width: 448px; height: 160px;">
+                <button
+                    class="absolute bottom-0 left-0 bg-transparent hover:bg-gray-500 hover:text-white font-bold py-2 px-4 rounded">
+                    01/01/2023
+                </button>
+                <button
+                    class="absolute bottom-0 right-0 bg-transparent hover:bg-gray-500 hover:text-white font-bold py-2 px-4 rounded">
+                    100k views
+                </button>
             </div>
+            <p class="text-gray-600 text-sm leading-5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque mi ac nisl lacinia, vitae
+                ullamcorper eros faucibus. Duis ullamcorper hendrerit urna, sit amet semper felis dictum quis. Vivamus nec
+                velit eu nibh vestibulum commodo ac felis blandit varius vitae sed sapien.
+            </p>
         </div>
         <div class="mx-auto max-w-md flex flex-col items-center">
-            <div class="flex flex-col items-center">
-                <img src="https://picsum.photos/400/300?random=" alt="Placeholder Image"
-                    class="w-full h-40 object-cover mb-4">
-                <p class="text-gray-600 text-sm leading-5">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque mi ac nisl lacinia, vitae
-                    ullamcorper eros faucibus. Duis ullamcorper hendrerit urna, sit amet semper felis dictum quis. Vivamus
-                    nec velit eu nibh vestibulum commodo ac felis blandit varius vitae sed sapien.
-                </p>
+            <div class="relative">
+                <img src="https://picsum.photos/213/160?random=" alt="Placeholder Image"
+                    class="w-full h-40 object-cover mb-4" style="width: 448px; height: 160px;">
+                <button
+                    class="absolute bottom-0 left-0 bg-transparent hover:bg-gray-500 hover:text-white font-bold py-2 px-4 rounded">
+                    01/01/2023
+                </button>
+                <button
+                    class="absolute bottom-0 right-0 bg-transparent hover:bg-gray-500 hover:text-white font-bold py-2 px-4 rounded">
+                    100k views
+                </button>
             </div>
+            <p class="text-gray-600 text-sm leading-5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque mi ac nisl lacinia, vitae
+                ullamcorper eros faucibus. Duis ullamcorper hendrerit urna, sit amet semper felis dictum quis. Vivamus nec
+                velit eu nibh vestibulum commodo ac felis blandit varius vitae sed sapien.
+            </p>
         </div>
         <div class="mx-auto max-w-md flex flex-col items-center">
-            <div class="flex flex-col items-center">
-                <img src="https://picsum.photos/400/300?random=" alt="Placeholder Image"
-                    class="w-full h-40 object-cover mb-4">
-                <p class="text-gray-600 text-sm leading-5">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque mi ac nisl lacinia, vitae
-                    ullamcorper eros faucibus. Duis ullamcorper hendrerit urna, sit amet semper felis dictum quis. Vivamus
-                    nec velit eu nibh vestibulum commodo ac felis blandit varius vitae sed sapien.
-                </p>
+            <div class="relative">
+                <img src="https://picsum.photos/213/160?random=" alt="Placeholder Image"
+                    class="w-full h-40 object-cover mb-4" style="width: 448px; height: 160px;">
+                <button
+                    class="absolute bottom-0 left-0 bg-transparent hover:bg-gray-500 hover:text-white font-bold py-2 px-4 rounded">
+                    01/01/2023
+                </button>
+                <button
+                    class="absolute bottom-0 right-0 bg-transparent hover:bg-gray-500 hover:text-white font-bold py-2 px-4 rounded">
+                    100k views
+                </button>
             </div>
+            <p class="text-gray-600 text-sm leading-5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque mi ac nisl lacinia, vitae
+                ullamcorper eros faucibus. Duis ullamcorper hendrerit urna, sit amet semper felis dictum quis. Vivamus nec
+                velit eu nibh vestibulum commodo ac felis blandit varius vitae sed sapien.
+            </p>
         </div>
         <div class="mx-auto max-w-md flex flex-col items-center">
-            <div class="flex flex-col items-center">
-                <img src="https://picsum.photos/400/300?random=" alt="Placeholder Image"
-                    class="w-full h-40 object-cover mb-4">
-                <p class="text-gray-600 text-sm leading-5">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque mi ac nisl lacinia, vitae
-                    ullamcorper eros faucibus. Duis ullamcorper hendrerit urna, sit amet semper felis dictum quis. Vivamus
-                    nec velit eu nibh vestibulum commodo ac felis blandit varius vitae sed sapien.
-                </p>
+            <div class="relative">
+                <img src="https://picsum.photos/213/160?random=" alt="Placeholder Image"
+                    class="w-full h-40 object-cover mb-4" style="width: 448px; height: 160px;">
+                <button
+                    class="absolute bottom-0 left-0 bg-transparent hover:bg-gray-500 hover:text-white font-bold py-2 px-4 rounded">
+                    01/01/2023
+                </button>
+                <button
+                    class="absolute bottom-0 right-0 bg-transparent hover:bg-gray-500 hover:text-white font-bold py-2 px-4 rounded">
+                    100k views
+                </button>
             </div>
+            <p class="text-gray-600 text-sm leading-5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque mi ac nisl lacinia, vitae
+                ullamcorper eros faucibus. Duis ullamcorper hendrerit urna, sit amet semper felis dictum quis. Vivamus nec
+                velit eu nibh vestibulum commodo ac felis blandit varius vitae sed sapien.
+            </p>
         </div>
         <div class="mx-auto max-w-md flex flex-col items-center">
-            <div class="flex flex-col items-center">
-                <img src="https://picsum.photos/400/300?random=" alt="Placeholder Image"
-                    class="w-full h-40 object-cover mb-4">
-                <p class="text-gray-600 text-sm leading-5">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque mi ac nisl lacinia, vitae
-                    ullamcorper eros faucibus. Duis ullamcorper hendrerit urna, sit amet semper felis dictum quis. Vivamus
-                    nec velit eu nibh vestibulum commodo ac felis blandit varius vitae sed sapien.
-                </p>
+            <div class="relative">
+                <img src="https://picsum.photos/213/160?random=" alt="Placeholder Image"
+                    class="w-full h-40 object-cover mb-4" style="width: 448px; height: 160px;">
+                <button
+                    class="absolute bottom-0 left-0 bg-transparent hover:bg-gray-500 hover:text-white font-bold py-2 px-4 rounded">
+                    01/01/2023
+                </button>
+                <button
+                    class="absolute bottom-0 right-0 bg-transparent hover:bg-gray-500 hover:text-white font-bold py-2 px-4 rounded">
+                    100k views
+                </button>
             </div>
+            <p class="text-gray-600 text-sm leading-5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque mi ac nisl lacinia, vitae
+                ullamcorper eros faucibus. Duis ullamcorper hendrerit urna, sit amet semper felis dictum quis. Vivamus nec
+                velit eu nibh vestibulum commodo ac felis blandit varius vitae sed sapien.
+            </p>
         </div>
         <div class="mx-auto max-w-md flex flex-col items-center">
-            <div class="flex flex-col items-center">
-                <img src="https://picsum.photos/400/300?random=" alt="Placeholder Image"
-                    class="w-full h-40 object-cover mb-4">
-                <p class="text-gray-600 text-sm leading-5">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque mi ac nisl lacinia, vitae
-                    ullamcorper eros faucibus. Duis ullamcorper hendrerit urna, sit amet semper felis dictum quis. Vivamus
-                    nec velit eu nibh vestibulum commodo ac felis blandit varius vitae sed sapien.
-                </p>
+            <div class="relative">
+                <img src="https://picsum.photos/213/160?random=" alt="Placeholder Image"
+                    class="w-full h-40 object-cover mb-4" style="width: 448px; height: 160px;">
+                <button
+                    class="absolute bottom-0 left-0 bg-transparent hover:bg-gray-500 hover:text-white font-bold py-2 px-4 rounded">
+                    01/01/2023
+                </button>
+                <button
+                    class="absolute bottom-0 right-0 bg-transparent hover:bg-gray-500 hover:text-white font-bold py-2 px-4 rounded">
+                    100k views
+                </button>
             </div>
+            <p class="text-gray-600 text-sm leading-5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque mi ac nisl lacinia, vitae
+                ullamcorper eros faucibus. Duis ullamcorper hendrerit urna, sit amet semper felis dictum quis. Vivamus nec
+                velit eu nibh vestibulum commodo ac felis blandit varius vitae sed sapien.
+            </p>
         </div>
         <div class="mx-auto max-w-md flex flex-col items-center">
-            <div class="flex flex-col items-center">
-                <img src="https://picsum.photos/400/300?random=" alt="Placeholder Image"
-                    class="w-full h-40 object-cover mb-4">
-                <p class="text-gray-600 text-sm leading-5">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque mi ac nisl lacinia, vitae
-                    ullamcorper eros faucibus. Duis ullamcorper hendrerit urna, sit amet semper felis dictum quis. Vivamus
-                    nec velit eu nibh vestibulum commodo ac felis blandit varius vitae sed sapien.
-                </p>
+            <div class="relative">
+                <img src="https://picsum.photos/213/160?random=" alt="Placeholder Image"
+                    class="w-full h-40 object-cover mb-4" style="width: 448px; height: 160px;">
+                <button
+                    class="absolute bottom-0 left-0 bg-transparent hover:bg-gray-500 hover:text-white font-bold py-2 px-4 rounded">
+                    01/01/2023
+                </button>
+                <button
+                    class="absolute bottom-0 right-0 bg-transparent hover:bg-gray-500 hover:text-white font-bold py-2 px-4 rounded">
+                    100k views
+                </button>
             </div>
+            <p class="text-gray-600 text-sm leading-5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque mi ac nisl lacinia, vitae
+                ullamcorper eros faucibus. Duis ullamcorper hendrerit urna, sit amet semper felis dictum quis. Vivamus nec
+                velit eu nibh vestibulum commodo ac felis blandit varius vitae sed sapien.
+            </p>
         </div>
         <div class="mx-auto max-w-md flex flex-col items-center">
-            <div class="flex flex-col items-center">
-                <img src="https://picsum.photos/400/300?random=" alt="Placeholder Image"
-                    class="w-full h-40 object-cover mb-4">
-                <p class="text-gray-600 text-sm leading-5">
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque mi ac nisl lacinia, vitae
-                    ullamcorper eros faucibus. Duis ullamcorper hendrerit urna, sit amet semper felis dictum quis. Vivamus
-                    nec velit eu nibh vestibulum commodo ac felis blandit varius vitae sed sapien.
-                </p>
+            <div class="relative">
+                <img src="https://picsum.photos/213/160?random=" alt="Placeholder Image"
+                    class="w-full h-40 object-cover mb-4" style="width: 448px; height: 160px;">
+                <button
+                    class="absolute bottom-0 left-0 bg-transparent hover:bg-gray-500 hover:text-white font-bold py-2 px-4 rounded">
+                    01/01/2023
+                </button>
+                <button
+                    class="absolute bottom-0 right-0 bg-transparent hover:bg-gray-500 hover:text-white font-bold py-2 px-4 rounded">
+                    100k views
+                </button>
             </div>
+            <p class="text-gray-600 text-sm leading-5">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque mi ac nisl lacinia, vitae
+                ullamcorper eros faucibus. Duis ullamcorper hendrerit urna, sit amet semper felis dictum quis. Vivamus nec
+                velit eu nibh vestibulum commodo ac felis blandit varius vitae sed sapien.
+            </p>
         </div>
+        
     </div>
     <div class="flex justify-center items-center h-20">
         <button class="w-40 bg-black hover:bg-gray-800 text-yellow-500 font-bold py-2 px-4">
