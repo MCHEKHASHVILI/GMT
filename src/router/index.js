cd86ccc3100e4abb77af3c5aca31e1225522cf96
zurikelass <zuragogberashvili93@gmail.com>
2023-04-14 22:00:16 +0400

    event id

--- src/router/index.js
+++ src/router/index.js
@@ -14,7 +14,7 @@ const router = createRouter({
       component: () => import('../views/Pages/About.vue')
     },
     {
-      path: '/events:zura',
+      path: '/events:id',
       name: 'events',
       component: () => import('../views/Pages/Events.vue')
     },
