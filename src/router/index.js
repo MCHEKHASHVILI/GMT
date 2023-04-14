766aa1d78e363baa429bbd1c29df1ca38b17dbe6
AstralBoy <baratashvili.beka@kiu.edu.ge>
2023-04-14 22:31:13 +0400

    Added router

--- src/router/index.js
+++ src/router/index.js
@@ -12,6 +12,11 @@ const router = createRouter({
       path: '/about',
       name: 'about',
       component: () => import('../views/Pages/About.vue')
+    },
+    {
+      path: '/News',
+      name: 'News',
+      component: () => import('../views/Pages/News.vue')
     }
   ]
 })
