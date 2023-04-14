540613858853b668663c073f109ecf89f0cebfb7
AstralBoy <baratashvili.beka@kiu.edu.ge>
2023-04-14 11:29:16 +0400

    Added brench

---
+++ src/views/Pages/News.vue
@@ -0,0 +1,114 @@
+<template>
+    <div class="flex justify-center items-center h-20">
+        <div class="text-center text-xl font-bold">
+            NEWS
+        </div>
+    </div>
+
+    <div class="grid grid-cols-3 gap-10">
+        <div class="mx-auto max-w-md flex flex-col items-center">
+            <div class="flex flex-col items-center">
+                <img src="https://picsum.photos/400/300?random=" alt="Placeholder Image"
+                    class="w-full h-40 object-cover mb-4">
+                <p class="text-gray-600 text-sm leading-5">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque mi ac nisl lacinia, vitae
+                    ullamcorper eros faucibus. Duis ullamcorper hendrerit urna, sit amet semper felis dictum quis. Vivamus
+                    nec velit eu nibh vestibulum commodo ac felis blandit varius vitae sed sapien.
+                </p>
+            </div>
+        </div>
+        <div class="mx-auto max-w-md flex flex-col items-center">
+            <div class="flex flex-col items-center">
+                <img src="https://picsum.photos/400/300?random=" alt="Placeholder Image"
+                    class="w-full h-40 object-cover mb-4">
+                <p class="text-gray-600 text-sm leading-5">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque mi ac nisl lacinia, vitae
+                    ullamcorper eros faucibus. Duis ullamcorper hendrerit urna, sit amet semper felis dictum quis. Vivamus
+                    nec velit eu nibh vestibulum commodo ac felis blandit varius vitae sed sapien.
+                </p>
+            </div>
+        </div>
+        <div class="mx-auto max-w-md flex flex-col items-center">
+            <div class="flex flex-col items-center">
+                <img src="https://picsum.photos/400/300?random=" alt="Placeholder Image"
+                    class="w-full h-40 object-cover mb-4">
+                <p class="text-gray-600 text-sm leading-5">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque mi ac nisl lacinia, vitae
+                    ullamcorper eros faucibus. Duis ullamcorper hendrerit urna, sit amet semper felis dictum quis. Vivamus
+                    nec velit eu nibh vestibulum commodo ac felis blandit varius vitae sed sapien.
+                </p>
+            </div>
+        </div>
+        <div class="mx-auto max-w-md flex flex-col items-center">
+            <div class="flex flex-col items-center">
+                <img src="https://picsum.photos/400/300?random=" alt="Placeholder Image"
+                    class="w-full h-40 object-cover mb-4">
+                <p class="text-gray-600 text-sm leading-5">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque mi ac nisl lacinia, vitae
+                    ullamcorper eros faucibus. Duis ullamcorper hendrerit urna, sit amet semper felis dictum quis. Vivamus
+                    nec velit eu nibh vestibulum commodo ac felis blandit varius vitae sed sapien.
+                </p>
+            </div>
+        </div>
+        <div class="mx-auto max-w-md flex flex-col items-center">
+            <div class="flex flex-col items-center">
+                <img src="https://picsum.photos/400/300?random=" alt="Placeholder Image"
+                    class="w-full h-40 object-cover mb-4">
+                <p class="text-gray-600 text-sm leading-5">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque mi ac nisl lacinia, vitae
+                    ullamcorper eros faucibus. Duis ullamcorper hendrerit urna, sit amet semper felis dictum quis. Vivamus
+                    nec velit eu nibh vestibulum commodo ac felis blandit varius vitae sed sapien.
+                </p>
+            </div>
+        </div>
+        <div class="mx-auto max-w-md flex flex-col items-center">
+            <div class="flex flex-col items-center">
+                <img src="https://picsum.photos/400/300?random=" alt="Placeholder Image"
+                    class="w-full h-40 object-cover mb-4">
+                <p class="text-gray-600 text-sm leading-5">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque mi ac nisl lacinia, vitae
+                    ullamcorper eros faucibus. Duis ullamcorper hendrerit urna, sit amet semper felis dictum quis. Vivamus
+                    nec velit eu nibh vestibulum commodo ac felis blandit varius vitae sed sapien.
+                </p>
+            </div>
+        </div>
+        <div class="mx-auto max-w-md flex flex-col items-center">
+            <div class="flex flex-col items-center">
+                <img src="https://picsum.photos/400/300?random=" alt="Placeholder Image"
+                    class="w-full h-40 object-cover mb-4">
+                <p class="text-gray-600 text-sm leading-5">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque mi ac nisl lacinia, vitae
+                    ullamcorper eros faucibus. Duis ullamcorper hendrerit urna, sit amet semper felis dictum quis. Vivamus
+                    nec velit eu nibh vestibulum commodo ac felis blandit varius vitae sed sapien.
+                </p>
+            </div>
+        </div>
+        <div class="mx-auto max-w-md flex flex-col items-center">
+            <div class="flex flex-col items-center">
+                <img src="https://picsum.photos/400/300?random=" alt="Placeholder Image"
+                    class="w-full h-40 object-cover mb-4">
+                <p class="text-gray-600 text-sm leading-5">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque mi ac nisl lacinia, vitae
+                    ullamcorper eros faucibus. Duis ullamcorper hendrerit urna, sit amet semper felis dictum quis. Vivamus
+                    nec velit eu nibh vestibulum commodo ac felis blandit varius vitae sed sapien.
+                </p>
+            </div>
+        </div>
+        <div class="mx-auto max-w-md flex flex-col items-center">
+            <div class="flex flex-col items-center">
+                <img src="https://picsum.photos/400/300?random=" alt="Placeholder Image"
+                    class="w-full h-40 object-cover mb-4">
+                <p class="text-gray-600 text-sm leading-5">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque mi ac nisl lacinia, vitae
+                    ullamcorper eros faucibus. Duis ullamcorper hendrerit urna, sit amet semper felis dictum quis. Vivamus
+                    nec velit eu nibh vestibulum commodo ac felis blandit varius vitae sed sapien.
+                </p>
+            </div>
+        </div>
+    </div>
+    <div class="flex justify-center items-center h-20">
+        <button class="w-40 bg-black hover:bg-gray-800 text-yellow-500 font-bold py-2 px-4">
+            MORE
+        </button>
+    </div>
+</template>
