9d53125b526a42836f636a70b08bc89b90dc1a86
zurikelass <zuragogberashvili93@gmail.com>
2023-04-18 16:57:26 +0400

    something changes about form

--- src/views/Pages/Events.vue
+++ src/views/Pages/Events.vue
@@ -10,65 +10,37 @@ import mainLayout from "../../layouts/mainLayout.vue";
 
     <!-- form -->
     <div>
-      <h2 class="w-screen flex pl-20 mb:text-xl font-medium mt-3">
+      <h2 class="flex pl-20 mb:text-xl font-medium mt-3">
         DON'T MISS OUT ON OUR EVENTS-STAY CONNECTED!
       </h2>
       <div
         class="mx-60 relative mb-[-245px] ml-[87px] pr-20 pl-20 bg-gray-50 p-2 border shadow-md"
       >
         <form class="flex flex-wrap justify-between z-0">
-          <div class="mb-4 mr-2">
-            <label class="block text-gray-700 font-bold mb-2"> Location </label>
-            <div class="relative">
-              <select
-                class="w-full bg-gray-100 border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
-              >
-                <option></option>
-                <option></option>
-              </select>
+          <div>
+            <input class="placeholder-gray-500 border" placeholder="Location">
+            
+              
               <div
                 class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700"
               >
-                <svg class="fill-current h-4 w-4" viewBox="0 0 20 20">
-                  <path
-                    d="M10 12l-5-5 1.41-1.41L10 9.18l3.59-3.59L15 7l-5 5z"
-                  />
-                </svg>
+                
               </div>
-            </div>
+           
           </div>
 
           <div class="mb-4 mr-2">
-            <label class="block text-gray-700 font-bold mb-2" for="date">
-              Date
-            </label>
-            <input
-              class="block appearance-none w-full bg-gray-100 border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
-              type="date"
-            />
+            <input class="placeholder-gray-500 border" placeholder="Date">
           </div>
 
-          <div class="mb-4 mr-2">
-            <label class="block text-gray-700 font-bold mb-2" for="type">
-              Type
-            </label>
-            <div class="relative">
-              <select
-                class="block appearance-none w-full bg-gray-100 border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
-              >
-                <option>Option 1</option>
-              </select>
-              <div
-                class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700"
-              >
-                <svg class="fill-current h-4 w-4" viewBox="0 0 20 20">
-                  <path
-                    d="M10 12l-5-5 1.41-1.41L10 9.18l3.59-3.59L15 7l-5 5z"
-                  />
-                </svg>
-              </div>
-            </div>
-          </div>
+           <div>
+            <input class="placeholder-gray-500 border" placeholder="Type" data-dropdown-toggle="dropdown" >
+
+           </div>
+           
+               
+            
+         
 
           <div class="justify-end mt-6">
             <button
