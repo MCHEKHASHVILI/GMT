3afbf30914bd818ccea3b63eb96be298e4076207
AstralBoy <baratashvili.beka@kiu.edu.ge>
2023-04-19 11:45:25 +0400

    Added date and views icon

--- src/views/Pages/News.vue
+++ src/views/Pages/News.vue
@@ -4,20 +4,17 @@
             NEWS
         </div>
     </div>
-
     <div class="grid grid-cols-3 gap-10">
         <div class="mx-auto max-w-md flex flex-col items-center">
-            <div class="relative">
+            <div class="relative w-full h-40 mb-4">
                 <img src="https://picsum.photos/213/160?random=" alt="Placeholder Image"
-                    class="w-full h-40 object-cover mb-4" style="width: 448px; height: 160px;">
-                <button
-                    class="absolute bottom-0 left-0 bg-transparent hover:bg-gray-500 hover:text-white font-bold py-2 px-4 rounded">
-                    01/01/2023
-                </button>
-                <button
-                    class="absolute bottom-0 right-0 bg-transparent hover:bg-gray-500 hover:text-white font-bold py-2 px-4 rounded">
-                    100k views
-                </button>
+                    class="absolute top-0 left-0 w-full h-full object-cover">
+                <span class="absolute bottom-0 left-0 bg-transparent text-white font-bold py-2 px-4 rounded">
+                    <i class="material-icons-outlined align-middle mr-2 text-2xl text-white">calendar_month</i> 01/01/2023
+                </span>
+                <span class="absolute bottom-0 right-0 bg-transparent text-white font-bold py-2 px-4 rounded">
+                    <i class="material-icons-outlined align-middle mr-2 text-2xl text-white">visibility</i> 100k
+                </span>
             </div>
             <p class="text-gray-600 text-sm leading-5">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque mi ac nisl lacinia, vitae
@@ -26,17 +23,15 @@
             </p>
         </div>
         <div class="mx-auto max-w-md flex flex-col items-center">
-            <div class="relative">
+            <div class="relative w-full h-40 mb-4">
                 <img src="https://picsum.photos/213/160?random=" alt="Placeholder Image"
-                    class="w-full h-40 object-cover mb-4" style="width: 448px; height: 160px;">
-                <button
-                    class="absolute bottom-0 left-0 bg-transparent hover:bg-gray-500 hover:text-white font-bold py-2 px-4 rounded">
-                    01/01/2023
-                </button>
-                <button
-                    class="absolute bottom-0 right-0 bg-transparent hover:bg-gray-500 hover:text-white font-bold py-2 px-4 rounded">
-                    100k views
-                </button>
+                    class="absolute top-0 left-0 w-full h-full object-cover">
+                <span class="absolute bottom-0 left-0 bg-transparent text-white font-bold py-2 px-4 rounded">
+                    <i class="material-icons-outlined align-middle mr-2 text-2xl text-white">calendar_month</i> 01/01/2023
+                </span>
+                <span class="absolute bottom-0 right-0 bg-transparent text-white font-bold py-2 px-4 rounded">
+                    <i class="material-icons-outlined align-middle mr-2 text-2xl text-white">visibility</i> 100k
+                </span>
             </div>
             <p class="text-gray-600 text-sm leading-5">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque mi ac nisl lacinia, vitae
@@ -45,17 +40,15 @@
             </p>
         </div>
         <div class="mx-auto max-w-md flex flex-col items-center">
-            <div class="relative">
+            <div class="relative w-full h-40 mb-4">
                 <img src="https://picsum.photos/213/160?random=" alt="Placeholder Image"
-                    class="w-full h-40 object-cover mb-4" style="width: 448px; height: 160px;">
-                <button
-                    class="absolute bottom-0 left-0 bg-transparent hover:bg-gray-500 hover:text-white font-bold py-2 px-4 rounded">
-                    01/01/2023
-                </button>
-                <button
-                    class="absolute bottom-0 right-0 bg-transparent hover:bg-gray-500 hover:text-white font-bold py-2 px-4 rounded">
-                    100k views
-                </button>
+                    class="absolute top-0 left-0 w-full h-full object-cover">
+                <span class="absolute bottom-0 left-0 bg-transparent text-white font-bold py-2 px-4 rounded">
+                    <i class="material-icons-outlined align-middle mr-2 text-2xl text-white">calendar_month</i> 01/01/2023
+                </span>
+                <span class="absolute bottom-0 right-0 bg-transparent text-white font-bold py-2 px-4 rounded">
+                    <i class="material-icons-outlined align-middle mr-2 text-2xl text-white">visibility</i> 100k
+                </span>
             </div>
             <p class="text-gray-600 text-sm leading-5">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque mi ac nisl lacinia, vitae
@@ -64,17 +57,15 @@
             </p>
         </div>
         <div class="mx-auto max-w-md flex flex-col items-center">
-            <div class="relative">
+            <div class="relative w-full h-40 mb-4">
                 <img src="https://picsum.photos/213/160?random=" alt="Placeholder Image"
-                    class="w-full h-40 object-cover mb-4" style="width: 448px; height: 160px;">
-                <button
-                    class="absolute bottom-0 left-0 bg-transparent hover:bg-gray-500 hover:text-white font-bold py-2 px-4 rounded">
-                    01/01/2023
-                </button>
-                <button
-                    class="absolute bottom-0 right-0 bg-transparent hover:bg-gray-500 hover:text-white font-bold py-2 px-4 rounded">
-                    100k views
-                </button>
+                    class="absolute top-0 left-0 w-full h-full object-cover">
+                <span class="absolute bottom-0 left-0 bg-transparent text-white font-bold py-2 px-4 rounded">
+                    <i class="material-icons-outlined align-middle mr-2 text-2xl text-white">calendar_month</i> 01/01/2023
+                </span>
+                <span class="absolute bottom-0 right-0 bg-transparent text-white font-bold py-2 px-4 rounded">
+                    <i class="material-icons-outlined align-middle mr-2 text-2xl text-white">visibility</i> 100k
+                </span>
             </div>
             <p class="text-gray-600 text-sm leading-5">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque mi ac nisl lacinia, vitae
@@ -83,17 +74,15 @@
             </p>
         </div>
         <div class="mx-auto max-w-md flex flex-col items-center">
-            <div class="relative">
+            <div class="relative w-full h-40 mb-4">
                 <img src="https://picsum.photos/213/160?random=" alt="Placeholder Image"
-                    class="w-full h-40 object-cover mb-4" style="width: 448px; height: 160px;">
-                <button
-                    class="absolute bottom-0 left-0 bg-transparent hover:bg-gray-500 hover:text-white font-bold py-2 px-4 rounded">
-                    01/01/2023
-                </button>
-                <button
-                    class="absolute bottom-0 right-0 bg-transparent hover:bg-gray-500 hover:text-white font-bold py-2 px-4 rounded">
-                    100k views
-                </button>
+                    class="absolute top-0 left-0 w-full h-full object-cover">
+                <span class="absolute bottom-0 left-0 bg-transparent text-white font-bold py-2 px-4 rounded">
+                    <i class="material-icons-outlined align-middle mr-2 text-2xl text-white">calendar_month</i> 01/01/2023
+                </span>
+                <span class="absolute bottom-0 right-0 bg-transparent text-white font-bold py-2 px-4 rounded">
+                    <i class="material-icons-outlined align-middle mr-2 text-2xl text-white">visibility</i> 100k
+                </span>
             </div>
             <p class="text-gray-600 text-sm leading-5">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque mi ac nisl lacinia, vitae
@@ -102,17 +91,15 @@
             </p>
         </div>
         <div class="mx-auto max-w-md flex flex-col items-center">
-            <div class="relative">
+            <div class="relative w-full h-40 mb-4">
                 <img src="https://picsum.photos/213/160?random=" alt="Placeholder Image"
-                    class="w-full h-40 object-cover mb-4" style="width: 448px; height: 160px;">
-                <button
-                    class="absolute bottom-0 left-0 bg-transparent hover:bg-gray-500 hover:text-white font-bold py-2 px-4 rounded">
-                    01/01/2023
-                </button>
-                <button
-                    class="absolute bottom-0 right-0 bg-transparent hover:bg-gray-500 hover:text-white font-bold py-2 px-4 rounded">
-                    100k views
-                </button>
+                    class="absolute top-0 left-0 w-full h-full object-cover">
+                <span class="absolute bottom-0 left-0 bg-transparent text-white font-bold py-2 px-4 rounded">
+                    <i class="material-icons-outlined align-middle mr-2 text-2xl text-white">calendar_month</i> 01/01/2023
+                </span>
+                <span class="absolute bottom-0 right-0 bg-transparent text-white font-bold py-2 px-4 rounded">
+                    <i class="material-icons-outlined align-middle mr-2 text-2xl text-white">visibility</i> 100k
+                </span>
             </div>
             <p class="text-gray-600 text-sm leading-5">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque mi ac nisl lacinia, vitae
@@ -121,17 +108,15 @@
             </p>
         </div>
         <div class="mx-auto max-w-md flex flex-col items-center">
-            <div class="relative">
+            <div class="relative w-full h-40 mb-4">
                 <img src="https://picsum.photos/213/160?random=" alt="Placeholder Image"
-                    class="w-full h-40 object-cover mb-4" style="width: 448px; height: 160px;">
-                <button
-                    class="absolute bottom-0 left-0 bg-transparent hover:bg-gray-500 hover:text-white font-bold py-2 px-4 rounded">
-                    01/01/2023
-                </button>
-                <button
-                    class="absolute bottom-0 right-0 bg-transparent hover:bg-gray-500 hover:text-white font-bold py-2 px-4 rounded">
-                    100k views
-                </button>
+                    class="absolute top-0 left-0 w-full h-full object-cover">
+                <span class="absolute bottom-0 left-0 bg-transparent text-white font-bold py-2 px-4 rounded">
+                    <i class="material-icons-outlined align-middle mr-2 text-2xl text-white">calendar_month</i> 01/01/2023
+                </span>
+                <span class="absolute bottom-0 right-0 bg-transparent text-white font-bold py-2 px-4 rounded">
+                    <i class="material-icons-outlined align-middle mr-2 text-2xl text-white">visibility</i> 100k
+                </span>
             </div>
             <p class="text-gray-600 text-sm leading-5">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque mi ac nisl lacinia, vitae
@@ -140,17 +125,15 @@
             </p>
         </div>
         <div class="mx-auto max-w-md flex flex-col items-center">
-            <div class="relative">
+            <div class="relative w-full h-40 mb-4">
                 <img src="https://picsum.photos/213/160?random=" alt="Placeholder Image"
-                    class="w-full h-40 object-cover mb-4" style="width: 448px; height: 160px;">
-                <button
-                    class="absolute bottom-0 left-0 bg-transparent hover:bg-gray-500 hover:text-white font-bold py-2 px-4 rounded">
-                    01/01/2023
-                </button>
-                <button
-                    class="absolute bottom-0 right-0 bg-transparent hover:bg-gray-500 hover:text-white font-bold py-2 px-4 rounded">
-                    100k views
-                </button>
+                    class="absolute top-0 left-0 w-full h-full object-cover">
+                <span class="absolute bottom-0 left-0 bg-transparent text-white font-bold py-2 px-4 rounded">
+                    <i class="material-icons-outlined align-middle mr-2 text-2xl text-white">calendar_month</i> 01/01/2023
+                </span>
+                <span class="absolute bottom-0 right-0 bg-transparent text-white font-bold py-2 px-4 rounded">
+                    <i class="material-icons-outlined align-middle mr-2 text-2xl text-white">visibility</i> 100k
+                </span>
             </div>
             <p class="text-gray-600 text-sm leading-5">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque mi ac nisl lacinia, vitae
@@ -159,17 +142,15 @@
             </p>
         </div>
         <div class="mx-auto max-w-md flex flex-col items-center">
-            <div class="relative">
+            <div class="relative w-full h-40 mb-4">
                 <img src="https://picsum.photos/213/160?random=" alt="Placeholder Image"
-                    class="w-full h-40 object-cover mb-4" style="width: 448px; height: 160px;">
-                <button
-                    class="absolute bottom-0 left-0 bg-transparent hover:bg-gray-500 hover:text-white font-bold py-2 px-4 rounded">
-                    01/01/2023
-                </button>
-                <button
-                    class="absolute bottom-0 right-0 bg-transparent hover:bg-gray-500 hover:text-white font-bold py-2 px-4 rounded">
-                    100k views
-                </button>
+                    class="absolute top-0 left-0 w-full h-full object-cover">
+                <span class="absolute bottom-0 left-0 bg-transparent text-white font-bold py-2 px-4 rounded">
+                    <i class="material-icons-outlined align-middle mr-2 text-2xl text-white">calendar_month</i> 01/01/2023
+                </span>
+                <span class="absolute bottom-0 right-0 bg-transparent text-white font-bold py-2 px-4 rounded">
+                    <i class="material-icons-outlined align-middle mr-2 text-2xl text-white">visibility</i> 100k
+                </span>
             </div>
             <p class="text-gray-600 text-sm leading-5">
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla scelerisque mi ac nisl lacinia, vitae
@@ -177,7 +158,7 @@
                 velit eu nibh vestibulum commodo ac felis blandit varius vitae sed sapien.
             </p>
         </div>
-        
+        <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons+Outlined" />
     </div>
     <div class="flex justify-center items-center h-20">
         <button class="w-40 bg-black hover:bg-gray-800 text-yellow-500 font-bold py-2 px-4">
