9c788e79739fec6a2fcc1003a0c29d052e5008b5
papunawereteli <papunawereteli20@gmail.com>
2023-04-20 03:52:37 +0400

    newFont

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <header class="flex h-[130px] justify-between items-center bg-black border-b-[1px] border-gray-600">
         <div class="flex flex-col items-center ml-[7%] justify-center">
-            <p class="text-white text-2xl">GMT HOSPITALITY</p>
+            <p class="text-white text-2xl font-mainFont">GMT HOSPITALITY</p>
             <p class="text-white text-[13px] font-fancyFont">Travellers insights</p>
         </div>
         
