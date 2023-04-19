b00b55f4951a3a259f6edeeac1154b8c33e351df
zurikelass <zuragogberashvili93@gmail.com>
2023-04-19 18:41:35 +0400

    makes card

--- src/views/Pages/Events.vue
+++ src/views/Pages/Events.vue
@@ -9,7 +9,7 @@ import mainLayout from "../../layouts/mainLayout.vue";
     <br />
 
     <!-- form -->
-    <div>
+    <!-- <div>
       <h2 class="flex pl-20 mb:text-xl font-medium mt-3">
         DON'T MISS OUT ON OUR EVENTS-STAY CONNECTED!
       </h2>
@@ -54,7 +54,7 @@ import mainLayout from "../../layouts/mainLayout.vue";
           alt="Chello"
         />
       </div>
-    </div>
+    </div> -->
     <br />
     <div
       class="flex items-center justify-center min-h-screen container mx-auto"
@@ -64,209 +64,204 @@ import mainLayout from "../../layouts/mainLayout.vue";
         class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8 pl-20 pr-20"
       >
         <!-- card -->
-        <div class="rounded-xl shadow-lg">
-          <div class="p-5 flex flex-col">
-            <div class="rounded-xl overflow-hidden">
-              <img
-                src="https://media.istockphoto.com/id/465732100/photo/african-jazz-musician-playing-the-saxophone.jpg?s=612x612&w=0&k=20&c=hCw5oibcUVJYSHqAWJQnD3AHpPATEdCZoA4TS8qqkng="
-                alt="jazzpic"
-              />
-            </div>
-            <h5 class="text-2xl md:text-xl font-medium mt-3">
-              Jazz up your night: Join us for live music and delicious dinning
-              at our restaurant!
-            </h5>
-            <div class="text-center px-6 pt-4 pb-2">
-              <span
-                class="inline-block bg-gray-200 rounded px-8 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-                >Jazz</span
-              >
-              <span
-                class="inline-block bg-gray-50 rounded px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-                >Price: 150 Gel</span
-              >
-              <span
-                class="inline-block bg-gray-50 rounded px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-                >March 25,2023 | 10:25</span
-              >
-            </div>
-            <div
-              href="#"
-              class="text-center bg-blue-100 text-black-700 py-2 rounded-lg font-semibold mt-4 hover:bg-grey-300 focus:scale-95 transition-all duration-200 ease-out"
-            >
-              VIEW DETAILS
-            </div>
+
+        <div class="max-w flex flex-col">
+          <div>
+            <img
+              src="https://media.istockphoto.com/id/465732100/photo/african-jazz-musician-playing-the-saxophone.jpg?s=612x612&w=0&k=20&c=hCw5oibcUVJYSHqAWJQnD3AHpPATEdCZoA4TS8qqkng="
+              alt="jazzpic"
+            />
           </div>
+          <h5 class="text-2xl md:text-xl font-medium mt-3">
+            Jazz up your night: Join us for live music and delicious dinning at
+            our restaurant!
+          </h5>
+          <div class="text-center px-6 pt-4 pb-2">
+            <span
+              class="inline-block bg-gray-200 px-8 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+              >Jazz</span
+            >
+            <span
+              class="inline-block bg-gray-50 px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+              >Price: 150 Gel</span
+            >
+            <span
+              class="inline-block bg-gray-50 px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+              >March 25,2023 | 10:25</span
+            >
+          </div>
+          <div
+            href="#"
+            class="text-center bg-gray-200 text-black-700 py-6 font-semibold mt hover:bg-grey-300 focus:scale-95"
+          >
+            VIEW DETAILS
+          </div>
+          
         </div>
-        <div class="rounded-xl shadow-lg">
-          <div class="p-5 flex flex-col">
-            <div class="rounded-xl overflow-hidden">
-              <img
-                src="https://media.istockphoto.com/id/465732100/photo/african-jazz-musician-playing-the-saxophone.jpg?s=612x612&w=0&k=20&c=hCw5oibcUVJYSHqAWJQnD3AHpPATEdCZoA4TS8qqkng="
-                alt="afrojazz"
-              />
-            </div>
-            <h5 class="text-2xl md:text-xl font-medium mt-3">
-              Jazz up your night: Join us for live music and delicious dinning
-              at our restaurant!
-            </h5>
-            <div class="text-center px-6 pt-4 pb-2">
-              <span
-                class="inline-block bg-gray-200 rounded px-8 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-                >Jazz</span
-              >
-              <span
-                class="inline-block bg-gray-50 rounded px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-                >Price: 150 Gel</span
-              >
-              <span
-                class="inline-block bg-gray-50 rounded px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-                >March 25,2023 | 10:25</span
-              >
-            </div>
-            <div
-              href="#"
-              class="text-center bg-blue-100 text-black-700 py-2 rounded-lg font-semibold mt-4 hover:bg-grey-300 focus:scale-95 transition-all duration-200 ease-out"
-            >
-              VIEW DETAILS
-            </div>
+        <div class="max-w flex flex-col">
+          <div>
+            <img
+              src="https://media.istockphoto.com/id/465732100/photo/african-jazz-musician-playing-the-saxophone.jpg?s=612x612&w=0&k=20&c=hCw5oibcUVJYSHqAWJQnD3AHpPATEdCZoA4TS8qqkng="
+              alt="jazzpic"
+            />
           </div>
+          <h5 class="text-2xl md:text-xl font-medium mt-3">
+            Jazz up your night: Join us for live music and delicious dinning at
+            our restaurant!
+          </h5>
+          <div class="text-center px-6 pt-4 pb-2">
+            <span
+              class="inline-block bg-gray-200 px-8 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+              >Jazz</span
+            >
+            <span
+              class="inline-block bg-gray-50 px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+              >Price: 150 Gel</span
+            >
+            <span
+              class="inline-block bg-gray-50 px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+              >March 25,2023 | 10:25</span
+            >
+          </div>
+          <div
+            href="#"
+            class="text-center bg-gray-200 text-black-700 py-6 font-semibold mt hover:bg-grey-300 focus:scale-95"
+          >
+            VIEW DETAILS
+          </div>
+          
         </div>
-        <div class="rounded-xl shadow-lg">
-          <div class="p-5 flex flex-col">
-            <div class="rounded-xl overflow-hidden">
-              <img
-                src="https://media.istockphoto.com/id/465732100/photo/african-jazz-musician-playing-the-saxophone.jpg?s=612x612&w=0&k=20&c=hCw5oibcUVJYSHqAWJQnD3AHpPATEdCZoA4TS8qqkng="
-                alt="afrojazz"
-              />
-            </div>
-            <h5 class="text-2xl md:text-xl font-medium mt-3">
-              Jazz up your night: Join us for live music and delicious dinning
-              at our restaurant!
-            </h5>
-            <div class="text-center px-6 pt-4 pb-2">
-              <span
-                class="inline-block bg-gray-200 rounded px-8 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-                >Jazz</span
-              >
-              <span
-                class="inline-block bg-gray-50 rounded px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-                >Price: 150 Gel</span
-              >
-              <span
-                class="inline-block bg-gray-50 rounded px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-                >March 25,2023 | 10:25</span
-              >
-            </div>
-            <div
-              href="#"
-              class="text-center bg-blue-100 text-black-700 py-2 rounded-lg font-semibold mt-4 hover:bg-grey-300 focus:scale-95 transition-all duration-200 ease-out"
-            >
-              VIEW DETAILS
-            </div>
+        <div class="max-w flex flex-col">
+          <div>
+            <img
+              src="https://media.istockphoto.com/id/465732100/photo/african-jazz-musician-playing-the-saxophone.jpg?s=612x612&w=0&k=20&c=hCw5oibcUVJYSHqAWJQnD3AHpPATEdCZoA4TS8qqkng="
+              alt="jazzpic"
+            />
           </div>
+          <h5 class="text-2xl md:text-xl font-medium mt-3">
+            Jazz up your night: Join us for live music and delicious dinning at
+            our restaurant!
+          </h5>
+          <div class="text-center px-6 pt-4 pb-2">
+            <span
+              class="inline-block bg-gray-200 px-8 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+              >Jazz</span
+            >
+            <span
+              class="inline-block bg-gray-50 px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+              >Price: 150 Gel</span
+            >
+            <span
+              class="inline-block bg-gray-50 px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+              >March 25,2023 | 10:25</span
+            >
+          </div>
+          <div
+            href="#"
+            class="text-center bg-gray-200 text-black-700 py-6 font-semibold mt hover:bg-grey-300 focus:scale-95"
+          >
+            VIEW DETAILS
+          </div>
+          
         </div>
-        <div class="rounded-xl shadow-lg">
-          <div class="p-5 flex flex-col">
-            <div class="rounded-xl overflow-hidden">
-              <img
-                src="https://media.istockphoto.com/id/465732100/photo/african-jazz-musician-playing-the-saxophone.jpg?s=612x612&w=0&k=20&c=hCw5oibcUVJYSHqAWJQnD3AHpPATEdCZoA4TS8qqkng="
-                alt="afrojazz"
-              />
-            </div>
-            <h5 class="text-2xl md:text-xl font-medium mt-3">
-              Jazz up your night: Join us for live music and delicious dinning
-              at our restaurant!
-            </h5>
-            <div class="text-center px-6 pt-4 pb-2">
-              <span
-                class="inline-block bg-gray-200 rounded px-8 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-                >Jazz</span
-              >
-              <span
-                class="inline-block bg-gray-50 rounded px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-                >Price: 150 Gel</span
-              >
-              <span
-                class="inline-block bg-gray-50 rounded px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-                >March 25,2023 | 10:25</span
-              >
-            </div>
-            <div
-              href="#"
-              class="text-center bg-blue-100 text-black-700 py-2 rounded-lg font-semibold mt-4 hover:bg-grey-300 focus:scale-95 transition-all duration-200 ease-out"
-            >
-              VIEW DETAILS
-            </div>
+        <div class="max-w flex flex-col">
+          <div>
+            <img
+              src="https://media.istockphoto.com/id/465732100/photo/african-jazz-musician-playing-the-saxophone.jpg?s=612x612&w=0&k=20&c=hCw5oibcUVJYSHqAWJQnD3AHpPATEdCZoA4TS8qqkng="
+              alt="jazzpic"
+            />
           </div>
+          <h5 class="text-2xl md:text-xl font-medium mt-3">
+            Jazz up your night: Join us for live music and delicious dinning at
+            our restaurant!
+          </h5>
+          <div class="text-center px-6 pt-4 pb-2">
+            <span
+              class="inline-block bg-gray-200 px-8 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+              >Jazz</span
+            >
+            <span
+              class="inline-block bg-gray-50 px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+              >Price: 150 Gel</span
+            >
+            <span
+              class="inline-block bg-gray-50 px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+              >March 25,2023 | 10:25</span
+            >
+          </div>
+          <div
+            href="#"
+            class="text-center bg-gray-200 text-black-700 py-6 font-semibold mt hover:bg-grey-300 focus:scale-95"
+          >
+            VIEW DETAILS
+          </div>
+          
         </div>
-        <div class="rounded-xl shadow-lg">
-          <div class="p-5 flex flex-col">
-            <div class="rounded-xl overflow-hidden">
-              <img
-                src="https://media.istockphoto.com/id/465732100/photo/african-jazz-musician-playing-the-saxophone.jpg?s=612x612&w=0&k=20&c=hCw5oibcUVJYSHqAWJQnD3AHpPATEdCZoA4TS8qqkng="
-                alt="afrojazz"
-              />
-            </div>
-            <h5 class="text-2xl md:text-xl font-medium mt-3">
-              Jazz up your night: Join us for live music and delicious dinning
-              at our restaurant!
-            </h5>
-            <div class="text-center px-6 pt-4 pb-2">
-              <span
-                class="inline-block bg-gray-200 rounded px-8 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-                >Jazz</span
-              >
-              <span
-                class="inline-block bg-gray-50 rounded px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-                >Price: 150 Gel</span
-              >
-              <span
-                class="inline-block bg-gray-50 rounded px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-                >March 25,2023 | 10:25</span
-              >
-            </div>
-            <div
-              href="#"
-              class="text-center bg-blue-100 text-black-700 py-2 rounded-lg font-semibold mt-4 hover:bg-grey-300 focus:scale-95 transition-all duration-200 ease-out"
-            >
-              VIEW DETAILS
-            </div>
+        <div class="max-w flex flex-col">
+          <div>
+            <img
+              src="https://media.istockphoto.com/id/465732100/photo/african-jazz-musician-playing-the-saxophone.jpg?s=612x612&w=0&k=20&c=hCw5oibcUVJYSHqAWJQnD3AHpPATEdCZoA4TS8qqkng="
+              alt="jazzpic"
+            />
           </div>
+          <h5 class="text-2xl md:text-xl font-medium mt-3">
+            Jazz up your night: Join us for live music and delicious dinning at
+            our restaurant!
+          </h5>
+          <div class="text-center px-6 pt-4 pb-2">
+            <span
+              class="inline-block bg-gray-200 px-8 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+              >Jazz</span
+            >
+            <span
+              class="inline-block bg-gray-50 px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+              >Price: 150 Gel</span
+            >
+            <span
+              class="inline-block bg-gray-50 px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+              >March 25,2023 | 10:25</span
+            >
+          </div>
+          <div
+            href="#"
+            class="text-center bg-gray-200 text-black-700 py-6 font-semibold mt hover:bg-grey-300 focus:scale-95"
+          >
+            VIEW DETAILS
+          </div>
+          
         </div>
-        <div class="rounded-xl shadow-lg">
-          <div class="p-5 flex flex-col">
-            <div class="rounded-xl overflow-hidden">
-              <img
-                src="https://media.istockphoto.com/id/465732100/photo/african-jazz-musician-playing-the-saxophone.jpg?s=612x612&w=0&k=20&c=hCw5oibcUVJYSHqAWJQnD3AHpPATEdCZoA4TS8qqkng="
-                alt="afrojazz"
-              />
-            </div>
-            <h5 class="text-2xl md:text-xl font-medium mt-3">
-              Jazz up your night: Join us for live music and delicious dinning
-              at our restaurant!
-            </h5>
-            <div class="text-center px-6 pt-4 pb-2">
-              <span
-                class="inline-block bg-gray-200 rounded px-8 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-                >Jazz</span
-              >
-              <span
-                class="inline-block bg-gray-50 rounded px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-                >Price: 150 Gel</span
-              >
-              <span
-                class="inline-block bg-gray-50 rounded px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
-                >March 25,2023 | 10:25</span
-              >
-            </div>
-            <div
-              href="#"
-              class="text-center bg-blue-100 text-black-700 py-2 rounded-lg font-semibold mt-4 hover:bg-grey-300 focus:scale-95 transition-all duration-200 ease-out"
-            >
-              VIEW DETAILS
-            </div>
+        <div class="max-w flex flex-col">
+          <div>
+            <img
+              src="https://media.istockphoto.com/id/465732100/photo/african-jazz-musician-playing-the-saxophone.jpg?s=612x612&w=0&k=20&c=hCw5oibcUVJYSHqAWJQnD3AHpPATEdCZoA4TS8qqkng="
+              alt="jazzpic"
+            />
           </div>
+          <h5 class="text-2xl md:text-xl font-medium mt-3">
+            Jazz up your night: Join us for live music and delicious dinning at
+            our restaurant!
+          </h5>
+          <div class="text-center px-6 pt-4 pb-2">
+            <span
+              class="inline-block bg-gray-200 px-8 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+              >Jazz</span
+            >
+            <span
+              class="inline-block bg-gray-50 px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+              >Price: 150 Gel</span
+            >
+            <span
+              class="inline-block bg-gray-50 px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2"
+              >March 25,2023 | 10:25</span
+            >
+          </div>
+          <div
+            href="#"
+            class="text-center bg-gray-200 text-black-700 py-6 font-semibold mt hover:bg-grey-300 focus:scale-95"
+          >
+            VIEW DETAILS
+          </div>
+          
         </div>
       </div>
     </div>
