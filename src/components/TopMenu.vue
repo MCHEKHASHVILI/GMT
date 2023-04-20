482f9f82c8004c6e9c8992b9d02c0b70344c4e6d
papunawereteli <papunawereteli20@gmail.com>
2023-04-20 19:21:25 +0400

    addedRouterLink

--- src/components/TopMenu.vue
+++ src/components/TopMenu.vue
@@ -1,10 +1,10 @@
 <template>
   <ul class="flex mr-[7%]">
-    <li class="text-white mx-5 cursor-pointer">BRANDS</li>
-    <li class="text-white mx-5 cursor-pointer">RESERVATION</li>
-    <li class="text-white mx-5 cursor-pointer">EVENTS</li>
-    <li class="text-white mx-5 cursor-pointer">NEWS</li>
-    <li class="text-white mx-5 cursor-pointer">ABOUT US</li>
-    <li class="text-white mx-5 cursor-pointer">GEO</li>
+    <li class="text-white mx-5 cursor-pointer"> <router-link to="/brands">BRANDS</router-link> </li>
+    <li class="text-white mx-5 cursor-pointer"> <router-link to="/reservation">RESERVATION</router-link> </li>
+    <li class="text-white mx-5 cursor-pointer"> <router-link to="/events">EVENTS</router-link> </li>
+    <li class="text-white mx-5 cursor-pointer"> <router-link to="/news">NEWS</router-link> </li>
+    <li class="text-white mx-5 cursor-pointer"> <router-link to="/about">ABOUT US</router-link> </li>
+    <li class="text-white mx-5 cursor-pointer"> <router-link to="/geo">GEO</router-link> </li>
   </ul>
 </template>
