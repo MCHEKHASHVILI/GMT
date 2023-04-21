748572de0dc6f4ebe30997955615c90c44235446
papunawereteli <papunawereteli20@gmail.com>
2023-04-22 03:54:28 +0400

    added variables

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,19 @@
+<script setup>
+
+  const name = "GMT HOSPITALITY"
+  const travellersInsights = "Travellers insights"
+
+</script>
+
+
 <template>
   <header
     class="flex h-[130px] justify-between items-center bg-black border-b-[1px] border-gray-600"
   >
     <div class="ml-[7%]">
       <a href="/" class="flex flex-col items-center">
-        <p class="text-white text-2xl font-mainFont">GMT HOSPITALITY</p>
-        <p class="text-white text-[13px] font-fancyFont">Travellers insights</p>
+        <p class="text-white text-2xl font-mainFont">{{ name }}</p>
+        <p class="text-white text-[13px] font-fancyFont">{{ travellersInsights }}</p>
       </a>
     </div>
   </header>
