0ffb5ec623721548f47ced91a6f8f6c5d6e935ec
papunawereteli <papunawereteli20@gmail.com>
2023-04-22 03:51:18 +0400

    created vars for components

--- src/components/TopMenu.vue
+++ src/components/TopMenu.vue
@@ -1,10 +1,20 @@
+
+<script setup>
+
+  const navbarComponents = [
+    {routerLink: "/brands", name: "BRANDS" },
+    {routerLink: "/reservation", name: "RESERVATION" },
+    {routerLink: "/events", name: "EVENTS" },
+    {routerLink: "/news", name: "NEWS" },
+    {routerLink: "/about", name: "ABOUT US" },
+    {routerLink: "/geo", name: "GEO" },
+    
+  ]
+
+</script>
+
 <template>
   <ul class="flex mr-[7%]">
-    <li class="text-white mx-5 cursor-pointer"> <router-link to="/brands">BRANDS</router-link> </li>
-    <li class="text-white mx-5 cursor-pointer"> <router-link to="/reservation">RESERVATION</router-link> </li>
-    <li class="text-white mx-5 cursor-pointer"> <router-link to="/events">EVENTS</router-link> </li>
-    <li class="text-white mx-5 cursor-pointer"> <router-link to="/news">NEWS</router-link> </li>
-    <li class="text-white mx-5 cursor-pointer"> <router-link to="/about">ABOUT US</router-link> </li>
-    <li class="text-white mx-5 cursor-pointer"> <router-link to="/geo">GEO</router-link> </li>
+    <li v-for="component in navbarComponents" class="text-white mx-5 cursor-pointer"><router-link :to="component.routerLink">{{ component.name }}</router-link></li>
   </ul>
 </template>
