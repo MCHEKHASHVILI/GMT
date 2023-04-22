d9614f54fea9f8981853a1732f59134e9d23878c
MARIAM TSERTSVADZE <maricercvadze259@gmail.com>
2023-04-22 15:44:18 +0400

    text in variable

--- src/views/Pages/Home.vue
+++ src/views/Pages/Home.vue
@@ -4,14 +4,13 @@ import mainLayout from '../../layouts/mainLayout.vue';
 <template>
   <main-layout>
     <p>
-      Lorem ipsum dolor sit amet consectetur adipisicing elit.  Assumenda sequi dolore molestiae! Expedita, 
-      eaque voluptatum aliquam iure quibusdam delectus praesentium fugit minima. Quae quis, odit eaque repellendus 
-      id dolor nulla maxime quo laboriosam nemo alias reiciendis labore mollitia quasi voluptatibus. Impedit aliquam 
-      quaerat doloribus harum asperiores magni minima! Provident animi error eum accusantium quia quae qui accusamus
-      vero excepturi itaque? Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione incidunt aut inventore id 
-      doloremque similique enim cumque. Deleniti iste pariatur voluptas ipsum non dolores recusandae eos perferendis 
-      molestias eaque? Eveniet ullam est harum assumenda, magnam quisquam nihil a facilis, quaerat tenetur rem labore 
-      impedit porro nisi totam veritatis eligendi iure nobis tempore nemo? Maxime unde in magni modi veritatis repellendus!
+    Home Page Content
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad maiores iure provident delectus praesentium labore nihil in veniam a totam quasi sunt aspernatur iusto architecto, vero perferendis modi suscipit vitae.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad maiores iure provident delectus praesentium labore nihil in veniam a totam quasi sunt aspernatur iusto architecto, vero perferendis modi suscipit vitae.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad maiores iure provident delectus praesentium labore nihil in veniam a totam quasi sunt aspernatur iusto architecto, vero perferendis modi suscipit vitae.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad maiores iure provident delectus praesentium labore nihil in veniam a totam quasi sunt aspernatur iusto architecto, vero perferendis modi suscipit vitae.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad maiores iure provident delectus praesentium labore nihil in veniam a totam quasi sunt aspernatur iusto architecto, vero perferendis modi suscipit vitae.
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad maiores iure provident delectus praesentium labore nihil in veniam a totam quasi sunt aspernatur iusto architecto, vero perferendis modi suscipit vitae.
   </p>
   </main-layout>
 </template>
