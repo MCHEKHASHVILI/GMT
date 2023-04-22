0a368da2ea130196b8a601930454cb7e44a2b0c1
MARIAM TSERTSVADZE <maricercvadze259@gmail.com>
2023-04-22 15:38:11 +0400

    text in variable

--- src/views/Pages/Home.vue
+++ src/views/Pages/Home.vue
@@ -1,16 +1,11 @@
 <script setup>
 import mainLayout from '../../layouts/mainLayout.vue';
+import Brands from './Brands.vue';
 </script>
 <template>
-  <main-layout>
+  <!-- <main-layout> -->
     <p>
-    Home Page Content
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad maiores iure provident delectus praesentium labore nihil in veniam a totam quasi sunt aspernatur iusto architecto, vero perferendis modi suscipit vitae.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad maiores iure provident delectus praesentium labore nihil in veniam a totam quasi sunt aspernatur iusto architecto, vero perferendis modi suscipit vitae.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad maiores iure provident delectus praesentium labore nihil in veniam a totam quasi sunt aspernatur iusto architecto, vero perferendis modi suscipit vitae.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad maiores iure provident delectus praesentium labore nihil in veniam a totam quasi sunt aspernatur iusto architecto, vero perferendis modi suscipit vitae.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad maiores iure provident delectus praesentium labore nihil in veniam a totam quasi sunt aspernatur iusto architecto, vero perferendis modi suscipit vitae.
-    Lorem ipsum dolor sit amet consectetur adipisicing elit. Ad maiores iure provident delectus praesentium labore nihil in veniam a totam quasi sunt aspernatur iusto architecto, vero perferendis modi suscipit vitae.
-    </p>
-  </main-layout>
+   <brands/>
+  </p>
+  <!-- </main-layout> -->
 </template>
