d65b3c7b99a51ba9bcb3add06c0b9c6fbaa727c9
Rikudo10 <giorgisamxaradze16@gmail.com>
2023-04-29 23:40:38 +0400

    test2

--- src/views/Pages/Employment.vue
+++ src/views/Pages/Employment.vue
@@ -83,7 +83,7 @@ const items = ref([
   <main-layout>
     <h1 class="text-center font-bold text-3xl pb-12 pt-[3%]">EMPLOYMENT</h1>
     <div class="w-full pb-[7%]">
-      <div class="grid gap-[2%] justify-center md:grid md:grid-cols-2 px-[5%] md:gap-[15px] md:pb-1.5 ">
+      <div class="grid gap-[2%] justify-center md:grid md:grid-cols-2 px-[10%] md:gap-[15px] md:pb-1.5 ">
         <div v-for="item in items" class="w-full p-6 bg-white border border-gray-300 pb-[8%] md:pb-0 relative md:relative">
           <div class="gap-[12%] md:gap-[7%] md:grid md:grid-cols-3 grid grid-cols-2 ">
             <img class="h-12" :src="item.logo">
@@ -91,7 +91,7 @@ const items = ref([
               <h2 class=" font-medium md:font-bold">{{item.title}}</h2>
               <h2 class="font-medium md:font-bold">{{item.name}}</h2>
             </div>
-            <div class="md:py-0 md:translate-x-[18%]">
+            <div class="md:py-0 md:translate-x-[10%]">
               <h2 class="font-bold md:font-bold pb-[15%] md:pt-[14%]">{{item.money}}</h2>
             </div>
           </div>
@@ -103,7 +103,7 @@ const items = ref([
             </div>
             <div class="grid gap-[20%] md:flex ">
             <p class="md:w-[50%]">{{item.date}}</p>
-              <div class="md:translate-y-[-40%] pb-[4%] md:w-[50%] md:translate-x-[14%]">
+              <div class="md:translate-y-[-40%] pb-[4%] md:w-[50%] md:translate-x-[10%]">
               <a href="#" class="hover:bg-black hover:text-yellow-500	 inline-block	 items-center px-[37%] md:px-[25%] py-4 text-sm  text-center text-black bg-zinc-100">
                 {{item.detail}}
               </a>
