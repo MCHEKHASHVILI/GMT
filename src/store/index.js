9339dc9b497e91fad5586489084cd69a7651ae6c
zurikelass <zuragogberashvili93@gmail.com>
2023-05-09 00:32:38 +0400

    working on modules

--- src/store/index.js
+++ src/store/index.js
@@ -3,13 +3,14 @@ import brandsModule from "./modules/brands"
 import employmentModule from "./modules/employment";
 import navbarModule from "./modules/navbar";
 import titlesModule from "./modules/titles";
-
+import eventsModule from "./modules/events";
 const store = createStore({
     modules: {
         "brands": brandsModule,
         "employment": employmentModule,
         "navbar": navbarModule,
         "titles": titlesModule,
+        "events": eventsModule,
     }
 })
 export default store
---
+++ src/store/modules/events.js
@@ -0,0 +1,27 @@
+const eventsModule ={
+    namespaced: true,
+     state () {
+        return {
+            events : [
+                {
+                    id: 1,
+                    type: "Jazz",
+                    price: "150 Gel",
+                    date: new Date("2023-03-21"),
+                    formattedDate: "March 21,2023 | 10:25",
+                    image:
+                      "https://media.istockphoto.com/id/465732100/photo/african-jazz-musician-playing-the-saxophone.jpg?s=612x612&w=0&k=20&c=hCw5oibcUVJYSHqAWJQnD3AHpPATEdCZoA4TS8qqkng=",
+                    title:
+                      " Jazz up your night: Join us for live music and delicious dinning at our restaurant!",
+                  },
+            ]
+        }
+     },
+     getters: {
+        events(state) {
+            return state.events
+        }
+    }
+
+}
+export default eventsModule
