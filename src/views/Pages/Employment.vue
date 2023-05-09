663f492be243eae33dc8f7ef182bda33ae07d743
Rikudo10 <giorgisamxaradze16@gmail.com>
2023-05-10 01:08:13 +0400

    Done

--- src/views/Pages/Employment.vue
+++ src/views/Pages/Employment.vue
@@ -8,20 +8,20 @@ const vacancies = computed(() => store.getters["employment/getVacancies"]);
 </script>
 <template>
   <main-layout>
-    <h1 class="text-center font-bold text-3xl pb-12 pt-[3%] uppercase">employment</h1> 
-    <div class="w-full pb-[60%] md:pb-[7%]">
+    <h1 class="text-center font-bold text-3xl pb-12 pt-8 uppercase">employment</h1> 
+    <div class="w-full pb-14 md:pb-16">
       <div
-        class="grid gap-[2%] justify-center md:grid md:grid-cols-2 px-[10%] md:gap-[15px] md:pb-1.5"
+        class="grid gap-10 justify-center md:grid md:grid-cols-2 px-12 md:px-32 md:gap-4 md:pb-1.5"
       >
         <div
           v-for="vacancy in vacancies"
-          class="w-full p-6 bg-white border border-gray-300 pb-[8%] md:pb-0 relative md:relative"
+          class="w-full p-6 bg-white border border-gray-300  md:pb-0 relative md:relative"
         >
           <div
-            class="gap-[12%] md:gap-[7%] md:grid md:grid-cols-3 grid grid-cols-2"
+            class="gap-x-10 gap-y-4 md:gap-10 md:grid md:grid-cols-3 grid grid-cols-2"
           >
             <img class="h-12" :src="vacancy.company.logo" />
-            <div class="translate-x-[-70%]">
+            <div class="-translate-x-28">
               <h2 class="font-medium md:font-bold whitespace-nowrap">
                 {{ vacancy.company.name }}
               </h2>
@@ -29,8 +29,8 @@ const vacancies = computed(() => store.getters["employment/getVacancies"]);
                 {{ vacancy.job.position }}
               </h2>
             </div>
-            <div class="md:py-0 md:translate-x-[10%]">
-              <h2 class="font-bold md:font-bold pb-[15%] md:pt-[14%]">
+            <div class="md:py-0 md:translate-x-8">
+              <h2 class="font-bold md:font-bold pb-4 md:pt-6">
                 {{ Object.values(vacancy.job.salary).join(" ") }}
               </h2>
             </div>
@@ -39,22 +39,22 @@ const vacancies = computed(() => store.getters["employment/getVacancies"]);
             {{ vacancy.job.title }}
           </p>
           <div class="">
-            <div class="pb-[3%] md:grid md:grid-cols-1">
+            <div class="pb-4 md:grid md:grid-cols-1">
               <p class="pb-1.5">
                 <span class="font-medium md:font-bold">{{
                   vacancy.job.type
                 }}</span>
-                <span class="pl-[5%]">{{ vacancy.job.hours }}</span>
+                <span class="pl-4">{{ vacancy.job.hours }}</span>
               </p>
             </div>
-            <div class="grid gap-[20%] md:flex">
+            <div class="grid gap-8 md:gap-28 md:flex">
               <p class="md:w-[50%]">{{ vacancy.publishedAt }}</p>
               <div
-                class="md:translate-y-[-40%] pb-[4%] md:w-[50%] md:translate-x-[10%]"
+                class="md:-translate-y-8 pb-6 md:w-1/2 md:translate-x-4"
               >
                 <router-link
                   :to="'/employment/' + vacancy.id"
-                  class="hover:bg-black hover:text-yellow-500 inline-block items-center px-[37%] md:px-[25%] py-4 text-base text-center text-black bg-zinc-100"
+                  class="hover:bg-black hover:text-yellow-500 inline-block items-center px-[37%] md:px-16 py-4 text-sm md:text-base text-center text-black bg-zinc-100"
                 >
                   VIEW DETAILS
                 </router-link>
