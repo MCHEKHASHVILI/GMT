a85fadae5628431ab917fca2dfe3c66cb9d06643
ana94-ana <aniabramishvili94@gmail.com>
2023-05-09 22:27:06 +0400

    add module

--- src/store/index.js
+++ src/store/index.js
@@ -5,13 +5,15 @@ import navbarModule from "./modules/navbar";
 import titlesModule from "./modules/titles";
 import aboutModule from "./modules/about";
 
+
 const store = createStore({
     modules: {
         "brands": brandsModule,
         "employment": employmentModule,
         "navbar": navbarModule,
         "titles": titlesModule,
-        "about" : aboutModule,
+        "about": aboutModule,
+        
     }
 })
 export default store
