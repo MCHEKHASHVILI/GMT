78f80850b427269a4a537f6fac0f5dbb913ad4b4
seteri <wpapuna5@gmail.com>
2023-05-17 01:58:10 +0400

    created gallery component. still a few things to do

--- src/views/Pages/BrandsContent.vue
+++ src/views/Pages/BrandsContent.vue
@@ -1,19 +1,13 @@
 <script>
+
 import mainLayout from '../../layouts/mainLayout.vue';
-import '@splidejs/vue-splide/css';
-import { ref, onMounted } from 'vue';
-import { mapGetters, mapActions } from 'vuex';
-import { Splide, SplideSlide } from '@splidejs/vue-splide';
-
-import {useStore} from "vuex";
-
-
+import {mapGetters, useStore} from 'vuex';
+import galleryComponent from "@/components/galleryComponent.vue";
+import {onMounted, ref} from "vue";
 
 export default {
   components: {
-    Splide,
-    SplideSlide,
-
+    galleryComponent,
     mainLayout,
   },
   computed: {
@@ -21,57 +15,16 @@ export default {
   },
 
 
+    setup() {
 
-  setup() {
-      const store = useStore()
+        const store = useStore()
 
-      const main = ref();
-    const thumbs = ref();
+        onMounted(() => {
+            store.dispatch("brands/fetchFrontPageData")
+            store.dispatch("brands/fetchBrandsData")
+        });
 
-
-
-    const mainOptions = {
-      type: 'loop',
-      rewind: true,
-      perPage: 1,
-      pagination: false,
-      fixedHeight: 740,
-    };
-
-    const thumbOptions = ref({
-      type: 'slide',
-      rewind: true,
-      perPage: 4,
-      pagination: false,
-      arrows: false,
-      gap: '0.6rem',
-      focus: 'center',
-      isNavigation: true,
-      updateOnMove: true,
-    });
-
-    const updateThumbOptions = () => {
-      thumbOptions.value.perPage = window.innerWidth >= 768 ? 4 : 1;
-    };
-
-    onMounted(() => {
-      const thumbsSplide = thumbs.value?.splide;
-
-        if (thumbsSplide) {
-        main.value?.sync(thumbsSplide);
-      }
-
-
-      updateThumbOptions();
-        store.dispatch("brands/fetchFrontPageData")
-        store.dispatch("brands/fetchBrandsData")
-
-        window.addEventListener('resize', updateThumbOptions);
-
-    });
-
-    return { mainOptions, thumbOptions, main, thumbs, };
-  },
+    },
 };
 </script>
 
@@ -115,20 +68,8 @@ export default {
 
 <!-- სურათების ნაწილი -->
 
-<section class="w-5/6 mt-24 md:mt-0 flex flex-col mb-28 items-center">
-    <h1 class="font-[arial] font-bold text-3xl mb-10">GALLERY</h1>
-    <Splide class="w-full md:inline-block hidden mb-3" :options="mainOptions" aria-label="My Favorite Images" ref="main">
-      <SplideSlide v-for="image in brandsContent[0]?.galerry" :key="image">
-        <img class="h-full w-full" :src="image" :alt="'Slide ' + image">
-      </SplideSlide>
-    </Splide>
+        <gallery-component :images-source="brandsContent[0]?.galerry"/>
 
-    <Splide class="w-full h-1/5" :options="thumbOptions" aria-label="My Favorite Images" ref="thumbs">
-      <SplideSlide class="h-44" v-for="image in brandsContent[0]?.galerry" :key="image" :class="[ 'opacity-75 active:opacity-100 active:border-0' ]">
-        <img class="w-full h-full" :src="image" :alt="'Thumbnail ' + image">
-      </SplideSlide>
-    </Splide>
-  </section>
 
 <!-- კონტაკტის ნაწილი -->
 
