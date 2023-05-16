917ededb4e28fd719373e265431778ff0104ded6
papunawereteli <papunawereteli20@gmail.com>
2023-05-16 04:16:11 +0400

    used api for brands. Still some things to be fixed

--- src/store/modules/brands.js
+++ src/store/modules/brands.js
@@ -6,36 +6,21 @@ const brandsModule = {
         return {
 
             frontPageApi: "https://gmt.javal.ge/wp-json/custom/v1/homepage/en",
+            brandsApi: "https://gmt.javal.ge/wp-json/custom/v1/brands/en",
+
+
+
+
+
+            brandsContent:[
+
+
+            ],
 
 
 
             brands: [
-                {
-                    id: 1,
-                    img: null,
-                    icon: null,
-                    name: null,
-                    description:
-                        null,
 
-                    mainImg: "https://i.imgur.com/EdMolig.png",
-                    conceptDescription: {
-                        firstPart:"The magnificent building of Funicular Complex was built in 1938. Since then it is an architectural landmark and the trademark of Tbilisi.The building itself was built by the project of Georgian architects Zakaria and Nadezhda Kurdiani (coauthor A.Valabuev).",
-                        secondPart: 'Funicular complex is a prominent venue in several Georgian cult movies such as: "Nag", "Day Last, Day First", and "Encounter in the Mountain".',
-                        lastPart: "Nowadays Funicular Complex is owned by GMT Group and includes 4 separate dining facilities: Tbilisi's legendary restaurant Funicular with its fascinating Ballroom, traditional Georgian restaurant Chela and café Funicular. Each outlet is conceptually different and unique from one another with its delicious cuisine, distinctive service and the cozy interior.",
-                    },
-
-                    galerry: ["https://media-cdn.tripadvisor.com/media/photo-o/12/07/9f/c4/restaurant-funicular.jpg", "https://sakurageorgia.com/storage/app/media/uploaded-files/51830210_1163410307162283_286887846684393472_o.jpg","https://sakurageorgia.com/storage/app/media/uploaded-files/59816191_1216595945177052_844218183291240448_o.jpg","https://sakurageorgia.com/storage/app/media/uploaded-files/51830210_1163410307162283_286887846684393472_o.jpg","https://sakurageorgia.com/storage/app/media/uploaded-files/59816191_1216595945177052_844218183291240448_o.jpg"]
-
-                },
-                {
-                    id: 2,
-                    img: "/src/assets/images/puri_guliani.png",
-                    icon: "/src/assets/logos/puri_guliani.svg",
-                    name: "puri guliani",
-                    description:
-                        "Lorem ipsum, lore dolor sit amet consectetur adipisicing elit. Repellat ullam id debitis nulla? Eligendi doloribus exercitationem at quod. Laudantium maxime illum, dolores quisquam autem explicabo sapiente natus quod nisi dolore.",
-                },
 
 
             ]
@@ -43,17 +28,42 @@ const brandsModule = {
     },
     getters: {
         brands: state => state.brands,
+        brandsContent: state => state.brandsContent,
 
     },
 
     mutations:{
         storeFrontPageData(state, payload){
             for(let a=0; a<payload.brands.length;a++){
-                state.brands[a].img = payload.brands[a].featured_image
-                state.brands[a].icon = payload.brands[a].logo
-                state.brands[a].name = payload.brands[a].title
-                state.brands[a].description = payload.brands[a].description
+                state.brands.push(
+                    {
+                        id: a+1,
+                        img: payload.brands[a].featured_image,
+                        icon: payload.brands[a].logo,
+                        name: payload.brands[a].title,
+                        description: payload.brands[a].description,
+                    }
+                )
 
+            }
+
+        },
+
+        storeBrandsData(state, payload){
+
+            for(let a=0; a<payload.brands.length;a++){
+
+                state.brandsContent.push(
+                    {
+                        mainImg: payload.brands[a].featured_image,
+                        conceptDescription: payload.brands[a].description,
+                        icon: payload.brands[a].logo,
+                        title: payload.brands[a].title,
+                        galerry: ["https://media-cdn.tripadvisor.com/media/photo-o/12/07/9f/c4/restaurant-funicular.jpg", "https://sakurageorgia.com/storage/app/media/uploaded-files/51830210_1163410307162283_286887846684393472_o.jpg","https://sakurageorgia.com/storage/app/media/uploaded-files/59816191_1216595945177052_844218183291240448_o.jpg","https://sakurageorgia.com/storage/app/media/uploaded-files/51830210_1163410307162283_286887846684393472_o.jpg","https://sakurageorgia.com/storage/app/media/uploaded-files/59816191_1216595945177052_844218183291240448_o.jpg"]
+
+                    }
+
+                )
 
 
             }
@@ -63,10 +73,14 @@ const brandsModule = {
     },
 
     actions: {
-
         fetchFrontPageData({commit, state}){
             axios.get(state.frontPageApi)
                 .then(result => commit("storeFrontPageData",result.data))
+        },
+
+        fetchBrandsData({commit, state}){
+            axios.get(state.brandsApi)
+                .then(result => commit("storeBrandsData", result.data))
 
         }
 
--- src/views/Pages/BrandsContent.vue
+++ src/views/Pages/BrandsContent.vue
@@ -2,59 +2,33 @@
 import mainLayout from '../../layouts/mainLayout.vue';
 import '@splidejs/vue-splide/css';
 import { ref, onMounted } from 'vue';
-import { mapGetters } from 'vuex';
+import { mapGetters, mapActions } from 'vuex';
 import { Splide, SplideSlide } from '@splidejs/vue-splide';
-import Phone from "../../components/icons/iconPhone.vue";
-import Address from "../../components/icons/iconAddress.vue";
-import Email from "../../components/icons/iconEmail.vue";
-import Facebook from "../..//components/icons/iconFacebook.vue";
-import Linkedin from "../../components/icons/iconLinkedin.vue";
+
+import {useStore} from "vuex";
+
+
 
 export default {
   components: {
     Splide,
     SplideSlide,
-    Phone,
-    Address,
-    Email,
-    Facebook,
-    Linkedin,
-    mainLayout
+
+    mainLayout,
   },
   computed: {
-    ...mapGetters('brands', ['brands']),
+    ...mapGetters('brands', ['brandsContent']),
   },
+
+
+
   setup() {
-    const main = ref();
+      const store = useStore()
+
+      const main = ref();
     const thumbs = ref();
 
-    const links = [
-      {
-        name: "ADDRESS",
-        content: "Mtatsminda plateau, 0114 Tbilisi, Georgia",
-        icon: Address,
-      },
-      {
-        name: "PHONE",
-        content: "+995 577 74 44 00",
-        icon: Phone,
-      },
-      {
-        name: "E-MAIL",
-        content: "info@funicular.ge",
-        icon: Email,
-      },
-      {
-        name: "FACEBOOK",
-        content: "Funicular \u{2022} ფუნიკულიორი",
-        icon: Facebook,
-      },
-      {
-        name: "LINKEDIN",
-        content: "Restaurant Funicular",
-        icon: Linkedin,
-      },
-    ];
+
 
     const mainOptions = {
       type: 'loop',
@@ -82,14 +56,21 @@ export default {
 
     onMounted(() => {
       const thumbsSplide = thumbs.value?.splide;
-      if (thumbsSplide) {
+
+        if (thumbsSplide) {
         main.value?.sync(thumbsSplide);
       }
+
+
       updateThumbOptions();
-      window.addEventListener('resize', updateThumbOptions);
+        store.dispatch("brands/fetchFrontPageData")
+        store.dispatch("brands/fetchBrandsData")
+
+        window.addEventListener('resize', updateThumbOptions);
+
     });
 
-    return { mainOptions, thumbOptions, main, thumbs, links };
+    return { mainOptions, thumbOptions, main, thumbs, };
   },
 };
 </script>
@@ -100,27 +81,19 @@ export default {
     <div class=" flex  flex-col  items-center mb-32">
 
 <div class="  flex  justify-center flex-col md:flex-row mb-16 md:mb-32 md:gap-32  gap-10">
-  <img class="md:w-1/2 " :src="brands[0].mainImg" alt="" />
+  <img class="md:w-1/2 " :src="brandsContent[0]?.mainImg" alt="" />
 
   <div class="flex-col pt-20   flex md:items-start items-center md:justify-end">
-    <img class=" mb-12 md:w-40" :src="brands[0].icon" alt="" />
+    <img class=" mb-12 md:w-40" :src="brandsContent[0]?.icon" alt="" />
     <h1 class="font-bold mb-8 text-3xl font-[arial]">
-      {{ brands[0].name.toUpperCase() }}
+        {{brandsContent[0]?.title}}
     </h1>
 
-    <div class="text-xl w-10/12  mb-4 ">
+    <div class="text-xl font-[arial] w-10/12  mb-4 ">
       <p class="font-[arial] ">
-        {{ brands[0].conceptDescription.firstPart }}
+        {{ brandsContent[0]?.conceptDescription }}
       </p>
-      <br />
-      <p class="font-[arial]">
-        {{ brands[0].conceptDescription.secondPart }}
-      </p>
-      <br />
-      <p class="font-[arial]">
-        {{ brands[0].conceptDescription.lastPart }}
-      </p>
-      <br />
+
     </div>
 
     <div class="md:w-8/12 w-10/12 md:flex-row flex-col text-lg flex gap-4">
@@ -129,7 +102,9 @@ export default {
       >
         MENU
       </button>
+
       <button
+
         class="bg-[#0b0b0b] transition py-3 duration-300 hover:opacity-80 text-[#d0a958] font-[arial] md:py-6 md:w-1/2"
       >
         GO TO WEBSITE
@@ -143,13 +118,13 @@ export default {
 <section class="w-5/6 mt-24 md:mt-0 flex flex-col mb-28 items-center">
     <h1 class="font-[arial] font-bold text-3xl mb-10">GALLERY</h1>
     <Splide class="w-full md:inline-block hidden mb-3" :options="mainOptions" aria-label="My Favorite Images" ref="main">
-      <SplideSlide v-for="image in brands[0].galerry" :key="image">
+      <SplideSlide v-for="image in brandsContent[0]?.galerry" :key="image">
         <img class="h-full w-full" :src="image" :alt="'Slide ' + image">
       </SplideSlide>
     </Splide>
 
     <Splide class="w-full h-1/5" :options="thumbOptions" aria-label="My Favorite Images" ref="thumbs">
-      <SplideSlide class="h-44" v-for="image in brands[0].galerry" :key="image" :class="[ 'opacity-75 active:opacity-100 active:border-0' ]">
+      <SplideSlide class="h-44" v-for="image in brandsContent[0]?.galerry" :key="image" :class="[ 'opacity-75 active:opacity-100 active:border-0' ]">
         <img class="w-full h-full" :src="image" :alt="'Thumbnail ' + image">
       </SplideSlide>
     </Splide>
@@ -159,37 +134,6 @@ export default {
 
 
 
-<div class="flex flex-col justify-center w-full items-center">
-        <h1 class="text-3xl font-[arial]  font-bold">CONTACT</h1>
-
-        <div class="flex md:flex-row flex-col h-full   w-[85%] py-5 items-center">
-
-          <div class="flex flex-col md:w-1/2 w-full mb-8	 gap-7">
-            <div v-for="link in links" class="flex">
-              <div
-                class="bg-[#e6e6e6] w-14 h-14 rounded-full flex justify-center items-center p-4"
-              >
-                <component :is="link.icon"></component>
-              </div>
-              <div class="flex flex-col md:w-full w-80 break-words justify-center pl-5">
-                <p class="font-semibold text-lg text-[#797878]">
-                  {{ link.name }}
-                </p>
-                <p class="text-lg  font-semibold">{{ link.content }}</p>
-              </div>
-            </div>
-          </div>
-
-          <div class="md:w-2/3 w-full h-full">
-            <iframe
-              class="w-full  h-52	 md:h-96 "
-              id="gmap_canvas"
-              src="https://maps.google.com/maps?q=Freedom Square, Aleksandr Pushkin Street, Tbilisi 0105, Georgia&t=&z=15&ie=UTF8&iwloc=&output=embed"
-            >
-            </iframe>
-          </div>
-        </div>
-      </div>
 </div>
   </mainLayout>
 
