a0c81a5f25aff0043859195572ffe63d331f14ef
DoomedAndria <bibiashviliandria@gmail.com>
2023-05-18 13:40:33 +0400

    fixed contact component

--- src/views/Pages/Contact.vue
+++ src/views/Pages/Contact.vue
@@ -1,43 +1,17 @@
 <script setup>
-import mainLayout from "../../layouts/mainLayout.vue";
-import Phone from '../../components/icons/iconPhone.vue'
-import Address from '../../components/icons/iconAddress.vue'
-import Email from '../../components/icons/iconEmail.vue'
-import Facebook from '../..//components/icons/iconFacebook.vue'
-import Linkedin from '../../components/icons/iconLinkedin.vue'
-
+import MainLayout from "@/layouts/mainLayout.vue";
 import ContactComponent from "@/components/ContactComponent.vue";
 
-const links = [
-    {
-        name: "ADDRESS",
-        content: "4 FREEDOM SQUARE,TBILISI 0105,GEORGIA",
-        icon: Address
-    },
-    {
-        name: "PHONE",
-        content: "+995 (32) 2988 988",
-        icon: Phone
-    },
-    {
-        name: "E-MAIL",
-        content: "GMTH0spitality@GMT.GE",
-        icon: Email
-    },
-    {
-        name: "FACEBOOK",
-        content: "GMTHospitality",
-        icon: Facebook
-    },
-    {
-        name: "LINKEDIN",
-        content: "GMTHospitality",
-        icon: Linkedin
-    },
-]
+const contactDetails = {
+    address: '4 FREEDOM SQUARE,TBILISI 0105,GEORGIA',
+    phone: '+995 (32) 2988 988',
+    email: 'GMTH0spitality@GMT.GE',
+    facebook: 'GMTHospitality',
+    linkedin: 'GMTHospitality'
+}
 </script>
 <template>
     <main-layout>
-        <ContactComponent :info="links"/>
+        <ContactComponent :contactDetails="contactDetails"/>
     </main-layout>
 </template>
