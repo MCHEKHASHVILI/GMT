ba7dd842c22a09dfc8ca51cb6d6656d0fdb8f732
AstralBoy <baratashvili.beka@kiu.edu.ge>
2023-05-19 00:17:29 +0400

    use api

--- src/store/modules/news.js
+++ src/store/modules/news.js
@@ -1,150 +1,93 @@
-export const newsModule = {
+import axios from "axios"
+
+const newsModule = {
   namespaced: true,
   state() {
     return {
+
+      newsApi: "https://gmt.javal.ge/wp-json/custom/v1/news/en",
+      singlenewsApi: "https://gmt.javal.ge/wp-json/custom/v1/news/104/en",
+
+
       news: [
-        {
-          id: 1,
-          img: '/src/assets/images/rest_rigi4.jpg',
-          icon: '/src/assets/images/rest_rigi4.jpg',
-          title: 'Tourism and COVID-19. Minimize losses, reacquire reserves',
-          content:
-            'Tourism will be one of the sectors most affected by COVID-19, in fact, for contributing to the contagion of the pandemic',
-          date: '15.03.2023',
-          views: 100,
-        },
-        {
-          id: 2,
-          img: '/src/assets/images/rest_rigi2.jpg',
-          icon: '/src/assets/images/rest_rigi2.jpg',
-          title: 'UNWTO updates projections of the impact of Covid-19 on tourism ',
-          content:
-            'Tourism will be one of the sectors most affected by COVID-19, in fact, for contributing to the contagion of the pandemic',
-          date: '16.03.2023',
-          views: 500,
-        },
-        {
-          id: 3,
-          img: '/src/assets/images/rest_rigi3.jpg',
-          icon: '/src/assets/images/rest_rigi3.jpg',
-          title: 'UNWTO updates projections of the impact of Covid-19 on tourism',
-          content:
-            'Tourism will be one of the sectors most affected by COVID-19, in fact, for contributing to the contagion of the pandemic',
-          date: '17.03.2023',
-          views: 750,
-        },
-        {
-          id: 4,
-          img: '/src/assets/images/rest_rigi1.jpg',
-          icon: '/src/assets/images/rest_rigi1.jpg',
-          title: 'Duis ullamcorper hendrerit urna, sit amet semper felis dictum quis.',
-          content:
-            'Nulla scelerisque mi ac nisl lacinia, vitae ullamcorper eros faucibus. Vivamus nec ex finibus, bibendum eros ut, tristique purus.',
-          date: '17.03.2023',
-          views: 750,
-        },
-        {
-          id: 5,
-          img: '/src/assets/images/rest_rigi5.jpg',
-          icon: '/src/assets/images/rest_rigi5.jpg',
-          title: 'Duis ullamcorper hendrerit urna, sit amet semper felis dictum quis.',
-          content:
-            'Nulla scelerisque mi ac nisl lacinia, vitae ullamcorper eros faucibus. Vivamus nec ex finibus, bibendum eros ut, tristique purus.',
-          date: '17.03.2023',
-          views: 750,
-        },
-        {
-          id: 6,
-          img: '/src/assets/images/rest_rigi6.jpg',
-          icon: '/src/assets/images/rest_rigi6.jpg',
-          title: 'Duis ullamcorper hendrerit urna, sit amet semper felis dictum quis.',
-          content:
-            'Nulla scelerisque mi ac nisl lacinia, vitae ullamcorper eros faucibus. Vivamus nec ex finibus, bibendum eros ut, tristique purus.',
-          date: '17.03.2023',
-          views: 750,
-        },
-        {
-          id: 7,
-          img: '/src/assets/images/puri_guliani.png',
-          icon: '/src/assets/images/puri_guliani.png',
-          title: 'Duis ullamcorper hendrerit urna, sit amet semper felis dictum quis.',
-          content:
-            'Nulla scelerisque mi ac nisl lacinia, vitae ullamcorper eros faucibus. Vivamus nec ex finibus, bibendum eros ut, tristique purus.',
-          date: '17.03.2023',
-          views: 750,
-        },
-        {
-          id: 8,
-          img: '/src/assets/images/funicular.png',
-          icon: '/src/assets/images/funicular.png',
-          title: 'Duis ullamcorper hendrerit urna, sit amet semper felis dictum quis.',
-          content:
-            'Nulla scelerisque mi ac nisl lacinia, vitae ullamcorper eros faucibus. Vivamus nec ex finibus, bibendum eros ut, tristique purus.',
-          date: '17.03.2023',
-          views: 750,
-        },
-        {
-          id: 9,
-          img: '/src/assets/images/rest_rigi1.jpg',
-          icon: '/src/assets/images/rest_rigi1.jpg',
-          title: 'Duis ullamcorper hendrerit urna, sit amet semper felis dictum quis.',
-          content:
-            'Nulla scelerisque mi ac nisl lacinia, vitae ullamcorper eros faucibus. Vivamus nec ex finibus, bibendum eros ut, tristique purus.',
-          date: '17.03.2023',
-          views: 750,
-        },
+
       ],
-      newscontent: {
-        paragraphs: [
-          `Lorem ipsum dolor sit amet
-                      consectetur, adipisicing elit. At fugiat
-                      tempora
-                      quaerat quisquam, quo iste quibusdam! Adipisci esse maxime saepe veniam id enim, illo, alias similique
-                      blanditiis ut nisi vero.
-                      Lorem ipsum dolor sit amet consectetur, adipisicing elit. At fugiat tempora quaerat quisquam, quo iste
-                      quibusdam! Adipisci esse maxime saepe veniam id eni illo,blanditiis ut nisi vero.
-                      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt ad nisi quia provident temporibus,
-                      quaerat similique facilis. Impedit eum vitae molestias  maxime iure.`,
-  
-          `Lorem ipsum dolor sit amet
-                      consectetur, adipisicing elit. At fugiat
-                      tempora
-                      quaerat quisquam, quo iste quibusdam! Adipisci esse maxime saepe veniam id enim, illo, alias similique
-                      blanditiis ut nisi vero.
-                      Lorem ipsum dolor sit amet consectetur, adipisicing elit. At fugiat tempora quaerat quisquam, quo iste
-                      quibusdam! Adipisci esse maxime saepe veniam id eni illo,blanditiis ut nisi vero.
-                      Lorem ipsum dolor sit amet consectetur adipisicing elit. Perspiciatis soluta culpa accusamus impedit sit
-                      cum dolore dolores nostrum nisi? Quibusdam.`,
-  
-          `Lorem ipsum dolor sit
-                      amet consectetur, adipisicing elit. At fugiat
-                      tempora
-                      quaerat quisquam, quo iste quibusdam! Adipisci esse maxime saepe veniam id enim, illo, alias similique
-                      blanditiis ut nisi vero.
-                      Lorem ipsum dolor sit amet consectetur, adipisicing elit. At fugiat tempora quaerat quisquam, quo iste
-                      quibusdam! Adipisci esse maxime saepe veniam id eni illo,blanditiis ut nisi vero.
-                      Lorem ipsum .`,
-  
-          `Lorem ipsum dolor sit
-                      amet
-                      consectetur, adipisicing elit. At fugiat
-                      tempora
-                      quaerat quisquam, quo iste quibusdam! Adipisci esse maxime saepe veniam id enim, illo, alias similique
-                      blanditiis ut nisi vero.`
-  
+
+      newscontent: [
+
       ]
     }
-  }
   },
   getters: {
     news: state => state.news,
-    getNewsById: (state) => (id) => {
-      return state.news.find((e) => e.id === id);
+    newscontent: state => state.newscontent
+  },
+
+
+  mutations: {
+    storeNewsData(state, payload) {
+      const article = payload.news[0]; 
+  
+      for (let i = 0; i < 9; i++) {
+        state.news.push({
+          id: i + 1,
+          img: article.featured_image,
+          excerpt: article.excerpt,
+          title: article.title,
+          publish_date: article.publish_date,
+          views: article.views
+        });
+      }
     },
-    getNewsContentByParagraph: (state) => (id) => {
-      const newsItem = state.news.find((e) => e.id === id);
-      return newsItem ? state.newscontent.paragraphs : [];
+    // storeNewsData(state, payload) {
+    //   for (let a = 0; a < payload.news.length; a++) {
+    //     state.news.push(
+    //       {
+    //         id: a + 1,
+    //         img: payload.news[a].featured_image,
+    //         excerpt: payload.news[a].excerpt,
+    //         title: payload.news[a].title,
+    //         publish_date: payload.news[a].publish_date,
+    //         views: payload.news[a].views
+    //       }
+    //     )
+
+    //   }
+
+    // },
+    storeSingleNewsData(state, payload) {
+
+      for (let a = 0; a < 9; a++) {
+
+        state.newscontent.push(
+          {
+            id: a+1,
+            mainImg: payload.singleNews[a].featured_image,
+            publish_date: payload.singleNews[a].publish_date,
+            views: payload.singleNews[a].views,
+            title: payload.singleNews[a].title,
+            content: payload.singleNews[a].content,
+          }
+
+        )
+
+
+      }
+
     },
+  },
+  actions: {
+    fetchNewsData({ commit, state }) {
+      axios.get(state.newsApi)
+        .then(result => commit("storeNewsData", result.data))
+    },
+
+    fetchSingleNewsData({ commit, state }) {
+      axios.get(state.singlenewsApi)
+        .then(result => commit("storeSingleNewsData", result.data))
+
+    }
+
   }
 }
 
