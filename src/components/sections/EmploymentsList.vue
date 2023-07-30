16262e4663dd8fa11cfa3c395915ec0476259bd6
MCHEKHASHVILI <m.chekhashvili@gmail.com>
2023-07-30 21:17:27 +0400

    fonts refactoring

--- src/components/sections/EmploymentsList.vue
+++ src/components/sections/EmploymentsList.vue
@@ -16,50 +16,53 @@ export default {
 }
 </script>
 <template>
-    <section class="grid grid-cols-2 gap-4">
+    <section class="mx-8 grid grid-cols-1 md:grid-cols-2 gap-4">
         <!-- Card Container -->
         <div v-for="item in data" class="w-full p-8 border border-[#707070] border-opacity-40">
             <!-- Card Content -->
-            <div class="flex flex-col space-y-2">
+            <div class="flex flex-col space-y-4">
                 <!-- Heading -->
-                <div class="flex flex-col md:flex-row">
+                <div class="flex flex-col md:flex-row space-y-4">
                     <!-- left / top -->
-                    <div class="flex flex-row justify-between space-x-8">
-                        <div class="w-48">
+                    <div class="flex flex-row justify-start space-x-4">
+                        <div class="w-36">
                             <img class="object-cover" :src="item?.featured_image"/>
                         </div>
-                        <div>
-                            <h2 class="font-medium md:font-bold whitespace-nowrap">{{ item?.title }}</h2>
-                            <h2 class="font-medium md:font-bold">{{ item?.position }}</h2>
+                        <div class="flex flex-col justify-between md:font-medium">
+                            <h1 class="text-lg md:text-2xl whitespace-nowrap inline-block align-top">{{ item?.title }}</h1>
+                            <h2 class="text-lg md:text-xl whitespace-nowrap inline-block align-bottom md:font-bold">{{ item?.position }}</h2>
                         </div>
                     </div>
                     <!-- right / down -->
-                    <div class="flex flex-1 justify-end">
-                        <h2 class="">{{ item?.salary }} ₾ per month</h2>
+                    <div class="flex flex-1 justify-start md:justify-end items-end">
+                        <h2 class="font-bold">
+                            <span class="text-xl md:text-2xl">{{ item?.salary }} ₾</span>&nbsp;
+                            <span class="text-lg md:text-xl">per month</span>
+                        </h2>
                     </div>
                 </div>
                 <!-- Content -->
-                <div>
+                <div class="text-lg md:text-xl">
                     {{ item?.excerpt }}
                 </div>
                 <!-- Footer -->
-                <div class="">
-                    <div class="pb-4 md:grid md:grid-cols-1">
-                        <p class="pb-1.5">
-                            <span class="font-medium md:font-bold">{{
-                                item?.working_time
-                            }}</span>
-                            <span class="pl-4">{{ item?.working_hours }}</span>
-                        </p>
-                    </div>
-                    <div class="grid gap-8 md:gap-28 md:flex">
-                        <p class="md:w-[50%]">{{ item?.publish_date }}</p>
-                        <div class="md:-translate-y-8 pb-6 md:w-1/2 md:translate-x-4">
-                            <router-link :to="{ name: 'EmploymentContent', params: { id: item?.id } }"
-                                class="hover:bg-black hover:text-yellow-500 inline-block items-center px-[37%] md:px-16 py-4 text-sm md:text-base text-center text-black bg-zinc-100">
-                                VIEW DETAILS
-                            </router-link>
+                <div class="flex flex-col md:flex-row md:justify-between items-center">
+                    <div class="flex flex-col justify-between w-full items-start space-y-4 md:space-y-2 mb-8 md:mb-0">
+                        <div class="flex flex-row justify-between space-x-2">
+                            <span class="text-xl font-bold">{{ item?.working_time }}</span>
+                            <span class="text-xl font-medium text-[#1D1A1A] text-opacity-70">{{ item?.working_hours }}</span>
                         </div>
+                        <span class="text-xl text-[#1D1A1A] text-opacity-70">{{ item?.publish_date }}</span>
+                    </div>
+                    <div class="flex w-full h-full justify-end">
+                        <router-link as="button"
+                            :to="{ name: 'EmploymentContent', params: { id: item?.id } }"
+                            :class="[
+                                'bg-[#F5F5F5] hover:bg-[#0B0B0B] text-[#0B0B0B] hover:text-[#CCA657] text-center',
+                                'h-full py-4 px-16 w-full md:w-auto',
+                            ]">
+                            <span class="text-lg uppercase whitespace-nowrap">view details</span>
+                        </router-link>
                     </div>
                 </div>
             </div>
