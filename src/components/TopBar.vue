20e6929f09ff909c762293366d1d347ff2b7cb9c
MCHEKHASHVILI <m.chekhashvili@gmail.com>
2023-08-02 21:36:52 +0400

    navbar name changed

--- src/components/TopBar.vue
+++ src/components/TopBar.vue
@@ -1,6 +1,6 @@
 
 <script setup>
-import DesktopNavbar from '@/components/navbars/DesktopNavbar.vue'
+import DesktopNavbar from '@/components/navbars/DesktopNavBar.vue'
 import MobileNavbar from '@/components/navbars/MobileNavbar.vue'
 </script>
 <template>
