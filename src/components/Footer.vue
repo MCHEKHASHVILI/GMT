5d7b8a7ce1b98edb0b2882f221743e5f769faade
MCHEKHASHVILI <m.chekhashvili@gmail.com>
2023-08-03 04:57:45 +0400

    Mobile footer fixed

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,31 +1,33 @@
 <script setup>
-import FooterMenuLinks from './FooterMenuLinks.vue'
-import FooterMenuLinksMobile from './FooterMenuLinksMobile.vue'
 
 import useHome from "@/composables/useHome"
 import useNavigation from "@/composables/useNavigation"
 
-const { logo } = useHome()
 const { footerLinks } = useNavigation()
 
 </script>
 <template>
     <footer class="bg-black text-white">
-        <div class="container mx-auto flex flex-col md:flex-row justify-between md:py-11">
-
-            <div class="w-full py-4">
-                <img src="/logos/sublogo.png" class="h-16 w-auto">
+        <div class="container mx-auto flex flex-col md:flex-row justify-between px-8 md:px-0 py-8 md:py-11">
+            <div class="w-full py-8 flex justify-around md:justify-start">
+                <RouterLink :to="{ name: 'Home' }" @click="toggleMenu">
+                <img :class="[
+                    'object-cover h-12',
+                    { 'invert': open }
+                ]" src="@/assets/logos/sublogo.png">
+                </RouterLink>
+                <!-- <img src="/logos/sublogo.png" class="h-16 w-auto"> -->
             </div>
 
-            <div class="w-full justify-end">
-                <div class="grid grid-cols-2 md:grid-cols-3 justify-items-stretch gap-y-2 text-lg">
-                    <router-link v-for="link in footerLinks" :to="{ name: link.name }" class="justify-self-start">{{
+            <div class="w-full justify-between md:justify-end">
+                <div class="grid grid-cols-2 md:grid-cols-3 gap-y-8 text-xl ">
+                    <router-link v-for="link in footerLinks" :to="{ name: link.name }" class="justify-self-start pl-14">{{
                         link.title }}</router-link>
-                    <div class="flex flex-col">
+                    <div class="flex flex-col pl-14">
                         <h2 class="text-xl">Follow us</h2>
-                        <div class="flex flex-row justify-content-start space-x-4 mt-2">
-                            <img class="w-12 md:w-15" src="/src/assets/icons/linkedin.svg">
-                            <img class="w-12 md:w-15" src="/src/assets/icons/facebook.svg">
+                        <div class="flex flex-row justify-content-start space-x-4 mt-2 py-2">
+                            <img class="w-14 md:w-15" src="@/assets/icons/linkedin.svg">
+                            <img class="w-14 md:w-15" src="@/assets/icons/facebook.svg">
                         </div>
                     </div>
                 </div>
