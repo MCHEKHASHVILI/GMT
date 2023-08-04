d5e6d0bfbe3243c98a65c7acea4f89cb0b2040d5
MCHEKHASHVILI <m.chekhashvili@gmail.com>
2023-08-04 23:01:30 +0400

    hero subtext shape fixed

--- src/views/Pages/Home.vue
+++ src/views/Pages/Home.vue
@@ -18,22 +18,44 @@ onMounted(() => getHomePage())
 
 </script>
 <template>
-  
   <mainLayout>
     <ApplicationLogo class="flex flex-col py-[70%] md:py-0 md:h-screen bg-black"
-      imgClass="m-auto md:w-2/5 w-11/12 text-center" />
+      imgClass="m-auto md:w-2/5 w-11/12 text-center opacity-100" />
     <div class="flex flex-col bg-black">
       <div class="text-[#FFFFFF] text-left md:text-center w-full px-4 md:px-0 md:w-3/5 md:mx-auto pb-4">
         <h3 class="text-center font-medium text-2xl md:text-3xl uppercase" v-html="subtitle"></h3>
-        <div class="text-lg mt-4" v-html="description"></div>
+        <div class="text-lg mt-4">
+            <div class="hidden md:block left"></div>
+            <div class="hidden md:block right"></div>
+            <div class="md:text-center" v-html="description"></div>
+        </div>
         <div class="flex flex-row space-x-4 justify-center text-2xl font-bold text-opacity-70 mt-8 md:mt-2">
           <img src="/src/assets/icons/dining.svg">
-          <h3 class="uppercase" v-html="sectionTitle"></h3>
+          <h3 class="uppercase font-medium opacity-70" v-html="sectionTitle"></h3>
         </div>
-        <img class="my-8 mx-auto" src="/src/assets/icons/arrow.svg">
+        <img class="my-8 mx-auto font-medium opacity-70" src="/src/assets/icons/arrow.svg">
       </div>
     </div>
     <BrandsList :brands="brands" />
   </mainLayout>
 </template>
+<style scopped>
+.left,
+.right {
+  background-color: transparent;
+  height: 12ex;
+  width: 25%;
+}
 
+.left {
+  clip-path: polygon(0 0, 100% 100%, 0 100%);
+  float: left;
+  shape-outside: polygon(0 0, 100% 100%, 0 100%);
+}
+
+.right {
+  clip-path: polygon(100% 0, 100% 100%, 0 100%);
+  float: right;
+  shape-outside: polygon(100% 0, 100% 100%, 0 100%);
+}
+</style>
