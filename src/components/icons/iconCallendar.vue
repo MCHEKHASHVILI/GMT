8c462700d3d503f098652ebb17ac7e547826709f
MCHEKHASHVILI <m.chekhashvili@gmail.com>
2023-08-04 18:43:56 +0400

    News Content icon bags fixed

--- src/components/icons/iconCallendar.vue
+++ src/components/icons/iconCallendar.vue
@@ -1,5 +1,7 @@
 <template>
-<svg xmlns="http://www.w3.org/2000/svg" width="13.303" height="15.203" viewBox="0 0 13.303 15.203">
-  <path id="calendar-solid" class="new-class" style="filter: invert(100%) sepia(1%) saturate(0%) hue-rotate(145deg) brightness(101%) contrast(101%);" d="M2.851.95V1.9H1.425A1.426,1.426,0,0,0,0,3.326V4.751H13.3V3.326A1.426,1.426,0,0,0,11.878,1.9H10.452V.95a.95.95,0,0,0-1.9,0V1.9h-3.8V.95a.95.95,0,0,0-1.9,0ZM13.3,5.7H0v8.077A1.426,1.426,0,0,0,1.425,15.2H11.878A1.426,1.426,0,0,0,13.3,13.778Z" fill="#fff"/>
-</svg>
-</template>
+  <svg xmlns="http://www.w3.org/2000/svg" width="13.303" height="15.203" viewBox="0 0 13.303 15.203">
+    <path id="calendar-solid" class="new-class"
+      style="filter: invert(100%) sepia(1%) saturate(0%) hue-rotate(145deg) brightness(101%) contrast(101%);"
+      d="M2.851.95V1.9H1.425A1.426,1.426,0,0,0,0,3.326V4.751H13.3V3.326A1.426,1.426,0,0,0,11.878,1.9H10.452V.95a.95.95,0,0,0-1.9,0V1.9h-3.8V.95a.95.95,0,0,0-1.9,0ZM13.3,5.7H0v8.077A1.426,1.426,0,0,0,1.425,15.2H11.878A1.426,1.426,0,0,0,13.3,13.778Z"
+      fill="#fff" />
+</svg></template>
--- src/views/Pages/NewsContent.vue
+++ src/views/Pages/NewsContent.vue
@@ -29,18 +29,18 @@ export default {
         <div class="flex flex-col px-4 md:px-0 md:mb-36 md:container md:mx-auto">
             <h1 class="text-center text-2xl md:text-3xl font-bold uppercase my-8 md:mt-16 md:mb-12">NEWS</h1>
             <img :src="content?.featured_image" class="object-cover" alt="">
-            <div class="flex flex-row items-center md:py-2">
-                <div class="flex items-center">
-                    <iconCallendar class="mr-2"/>
-                    <span> {{ content?.publish_date }} </span>
+            <div class="flex flex-row items-center py-4 md:py-2 space-x-4">
+                <div class="flex items-center justify-start space-x-2">
+                    <span><i class="fas fa-eye"></i></span>
+                    <span>{{ content?.publish_date }}</span>
                 </div>
-                <div class="py-3 md:py-0 px-6 flex items-center">
-                    <iconEye class="mr-2"/>
+                <div class="flex items-center justify-start space-x-2">
+                    <span><i class="fas fa-calendar"></i></span>
                     <span> {{ content?.views }} </span>
                 </div>
                 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" />
             </div>
-            <h2 class="text-lg md:text-3xl font-bold mb-4">{{ content?.title }}</h2>
+            <h2 class="text-lg md:text-3xl font-bold mb-4" v-html="content?.title"></h2>
             <div class="text-lg md:text-xl text-left mb-20 md:mb-4" v-html="content?.content"></div>
         </div>
     </main-layout>
