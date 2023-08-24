a85cd73c9af5c2bc9cf55733e83debbe4a1e611c
MCHEKHASHVILI <m.chekhashvili@gmail.com>
2023-08-24 19:13:53 +0400

    events updated

--- src/components/sections/EventsList.vue
+++ src/components/sections/EventsList.vue
@@ -1,7 +1,42 @@
-<script setup>
-defineProps({
-    events: Array
-})
+<script>
+import { mapGetters, mapActions } from 'vuex'
+import IconExit from "@/components/icons/iconExit.vue"
+export default {
+    props: {
+        events: Array
+    },
+    components: { IconExit },
+    computed: {
+        name: {
+            get() {
+                return this.$store.getters['events/name']
+            },
+            set(value) {
+                this.$store.commit('events/SET_NAME', value)
+            }
+        },
+        phone: {
+            get() {
+                return this.$store.getters['events/phone']
+            },
+            set(value) {
+                this.$store.commit('events/SET_PHONE', value)
+            }
+        },
+        email: {
+            get() {
+                return this.$store.getters['events/email']
+            },
+            set(value) {
+                this.$store.commit('events/SET_EMAIL', value)
+            }
+        },
+        ...mapGetters('events', ['booking'])
+    },
+    methods: {
+        ...mapActions('events', ['toggleBooking', 'book'])
+    }
+}
 </script>
 <template>
     <section class="grid md:grid-cols-3 gap-4">
@@ -24,14 +59,43 @@ defineProps({
                     </div>
                 </div>
                 <div class="flex flex-row justify-between space-x-4 text-[#0B0B0B] text-lg whitespace-nowrap">
-                    <router-link class="w-full p-4 bg-[#F5F5F5] hover:bg-[#0B0B0B] hover:text-white text-center" :to="{ name: 'EventsContent', params: { id: event.id } }">
+                    <router-link class="w-full p-4 bg-[#F5F5F5] hover:bg-[#0B0B0B] hover:text-white text-center"
+                        :to="{ name: 'EventsContent', params: { id: event.id } }">
                         <span class="uppercase">view details</span>
                     </router-link>
-                    <button class="w-full p-4 bg-[#0B0B0B] text-[#CCA657] hover:bg-[#444141]" @click.prevent="">
+                    <button class="w-full p-4 bg-[#0B0B0B] text-[#CCA657] hover:bg-[#444141]"
+                        @click.prevent="toggleBooking(event)">
                         <span class="uppercase">book now</span>
                     </button>
+
+                    <Teleport to="body">
+                        <div v-if="!!booking" class="absolute inset-0 bg-black bg-opacity-25">
+                            <div class="h-screen flex items-end md:items-center justify-center">
+                                <div class="h-3/4 md:h-auto md:py-4 w-full md:max-w-xl md:mx-auto bg-[#FFFFFF]">
+                                    <div class="container mx-auto flex flex-col justify-start space-y-8 ">
+                                        <div class="flex flex-row justify-between p-4">
+                                            <h3 class="w-full text-xl text-[#1D1A1A] uppercase">booking</h3>
+                                            <div class="w-full flex flex-row justify-end">
+                                                <IconExit @click.prevent="toggleBooking(null)"
+                                                    class="justify-end w-8 -mr-2" />
+                                            </div>
+                                        </div>
+                                        <form class="flex flex-col justify-between space-y-4 min-h-full p-2"
+                                            @submit.prevent="book">
+                                            <input type="text" v-model="name" class="w-full min-h-full bg-transparent border border-[#707070] border-opacity-50 px-4 py-4 text-[#0B0B0B] text-opacity-70 text-lg" placeholder="Full Name" />
+                                            <input type="text" v-model="phone" class="w-full min-h-full bg-transparent border border-[#707070] border-opacity-50 px-4 py-4 text-[#0B0B0B] text-opacity-70 text-lg" placeholder="Phone"  />
+                                            <input type="email" v-model="email" class="w-full min-h-full bg-transparent border border-[#707070] border-opacity-50 px-4 py-4 text-[#0B0B0B] text-opacity-70 text-lg" placeholder="Email"  />
+                                            <button class="w-full min-h-full bg-[#0B0B0B] px-2 py-4" type="submit">
+                                                <span class="text-[#CCA657] uppercase whitespace-nowrap">book</span>
+                                            </button>
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </Teleport>
+
                 </div>
             </div>
         </div>
-    </section>
-</template>
+</section></template>
--- src/store/modules/events.js
+++ src/store/modules/events.js
@@ -10,10 +10,19 @@ const eventsModule = {
     cover: null,
 
     page: null,
-    next_page: null,
     per_page: 5,
     total_pages: null,
     loading: false,
+
+    types: [],
+    type: null,
+    date: null,
+    filter_bar: false,
+
+    booking: null,
+    name: null,
+    phone: null,
+    email: null,
   },
   getters: {
     events: ({ events }) => events,
@@ -21,10 +30,21 @@ const eventsModule = {
     title: ({ title }) => title,
     subtitle: ({ subtitle }) => subtitle,
     cover: ({ cover }) => cover,
-    
+
     loading: ({ loading }) => loading,
-    next_page: ({ next_page }) => next_page,
-    params: ({ per_page, next_page }) => (next_page) ? { params: { per_page, page: next_page } } : { params: { per_page } }
+    next_page: (state) => ((state.total_pages - state.page) > 0) ? state.page + 1 : null,
+    params: ({ per_page }, { next_page }) => (next_page) ? { params: { per_page,  page: next_page,  } } : { params: { per_page } },
+    filtered_params: ({ type, date }, { params }) => (type || date) ? { params: { event_type: type, date, ...params.params } } : { ...params },
+
+    types: ({ types }) => types,
+    type: ({ type }) => type,
+    date: ({ date }) => date,
+    filter_bar: ({ filter_bar }) => filter_bar,
+    
+    booking: ({ booking }) => booking,
+    name: ({ name }) => name,
+    phone: ({ phone }) => phone,
+    email: ({ email }) => email,
   },
   mutations: {
     'SET_EVENTS': (state, { more, data }) => (!!more) ? state.events.push(...data) : state.events = [...data],
@@ -32,24 +52,33 @@ const eventsModule = {
     'SET_EVENT': (state, payload) => state.event = payload,
     'SET_SUBTITLE': (state, payload) => state.subtitle = payload,
     'SET_COVER': (state, payload) => state.cover = payload,
-    
+
     'SET_PAGE': (state, payload) => (payload) ? state.page = payload : state.page = null,
-    'SET_NEXT_PAGE': (state) => ((state.total_pages - state.page) > 0) ? state.next_page = state.page + 1 : state.next_page = null,
     'SET_TOTAL_PAGES': (state, payload) => (payload) ? state.total_pages = payload : state.total_pages = null,
     'SET_LOADING': (state, payload) => state.loading = (payload),
+
+    'SET_FILTER_BAR': (state, payload) => state.filter_bar = (payload),
+    'SET_TYPES': (state, payload) => state.types = payload,
+    'SET_TYPE': (state, payload) => state.type = payload,
+    'SET_DATE': (state, payload) => state.date = payload,
+
+    'SET_BOOKING': (state, payload) => state.booking = payload,
+    'SET_NAME': (state, payload) => state.name = payload,
+    'SET_PHONE': (state, payload) => state.phone = payload,
+    'SET_EMAIL': (state, payload) => state.email = payload,
   },
   actions: {
     getEvents: async ({ commit, getters }) => {
 
       commit('SET_LOADING', true)
+      commit('SET_PAGE', 0)
       let { data } = await axios.get('events', getters.params)
-      commit('SET_EVENTS', { more: false, data: data.events })
       commit('SET_SUBTITLE', data.subtitle)
       commit('SET_COVER', data.featured_image)
       commit('SET_PAGE', data.page)
       commit('SET_TOTAL_PAGES', data.total_pages)
-      commit('SET_NEXT_PAGE')
       commit('SET_TITLE', data.title)
+      commit('SET_EVENTS', { more: false, data: data.events })
       commit('SET_LOADING', false)
 
     },
@@ -66,11 +95,45 @@ const eventsModule = {
       commit('SET_LOADING', true)
       let { data } = await axios.get('events', getters.params)
       commit('SET_EVENTS', { more: true, data: data.events })
-      commit('SET_PAGE', data.page )
-      commit('SET_NEXT_PAGE')
+      commit('SET_PAGE', data.page)
       commit('SET_LOADING', false)
 
-    }
+    },
+    toggleFilterBar: ({ commit, getters }) => {
+      commit('SET_FILTER_BAR', !getters.filter_bar)
+      window.scrollTo({ top: 0 })
+      document.body.classList.toggle('overflow-hidden')
+      document.body.classList.toggle('touch-none')
+    },
+    toggleBooking: ({ commit, getters }, event) => {
+      commit('SET_BOOKING', (!!event) ? event?.id : null)
+      window.scrollTo({ top: 0 })
+      document.body.classList.toggle('overflow-hidden')
+      document.body.classList.toggle('touch-none')
+    },
+    setTypes: async({ commit }) => {
+      commit('SET_TYPES', [
+        { id: 'JAZZ', name: 'JAZZ' },
+        { id: 'other', name: 'other' },
+        { id: null, name: 'null' },
+      ])
+    },
+    filterEvents: async ({ commit, getters }) => {
+      commit('SET_LOADING', true)
+      let { data } = await axios.get('events', getters.filtered_params)
+      let { page, title, subtitle, featured_image, total_pages, events } = data
+      commit('SET_PAGE', page )
+      commit('SET_TITLE', title )
+      commit('SET_SUBTITLE', subtitle )
+      commit('SET_COVER', featured_image )
+      commit('SET_TOTAL_PAGES', total_pages )
+      commit('SET_EVENTS', { more: false, data: events } )
+      commit('SET_LOADING', false )
+    },
+    book: async({ commit, getters, dispatch }) => {
+      console.log('need book endpoint')
+      dispatch('toggleBooking', null)
+    },
   },
 }
 
